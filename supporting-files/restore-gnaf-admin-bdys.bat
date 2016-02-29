b6dca5b8a1865bd04a5ea138281cf50d70b0b95d
Hugh Saalmans <hugh.saalmans@iag.com.au>
2016-03-01 07:20:04 +1100

    added postgis extension creation

--- supporting-files/restore-gnaf-admin-bdys.bat
+++ supporting-files/restore-gnaf-admin-bdys.bat
@@ -1,4 +1,7 @@
-"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d psma_201602 -p 5432 -U postgres "C:\git-repos\GitHub\gnaf.dmp"
-"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d psma_201602 -p 5432 -U postgres "C:\git-repos\GitHub\admin-bdys.dmp"
+
+psql -d test -p 5434 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+
+"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d test -p 5434 -U postgres "C:\minus34\GitHub\gnaf.dmp"
+"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d test -p 5434 -U postgres "C:\minus34\GitHub\admin-bdys.dmp"
 
 pause
