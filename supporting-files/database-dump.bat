4ee39345f9ec95fbd2ab6fd2e0b889740cb1e83d
Hugh Saalmans <hugh.saalmans@iag.com.au>
2016-02-29 21:27:29 +1100

    removing old files

--- supporting-files/database-dump.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\PostgreSQL\9.4\bin\pg_dump" -Fc -d gnaf_test -n gnaf -p 5433 -U postgres > "C:\minus34\gnaf\gnaf.dmp"
-"C:\Program Files\PostgreSQL\9.4\bin\pg_dump" -Fc -d gnaf_test -n admin_bdys -p 5433 -U postgres > "C:\minus34\gnaf\admin_bdys.dmp"
-pause
