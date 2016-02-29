bae0d894d2a347fff8dbdf2f9f5423070e789aa8
Hugh Saalmans <hugh.saalmans@iag.com.au>
2016-03-01 07:30:56 +1100

    changed default db name

--- supporting-files/restore-gnaf-admin-bdys.bat
+++ supporting-files/restore-gnaf-admin-bdys.bat
@@ -1,7 +1,7 @@
 
-psql -d test -p 5434 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql -d psma_201602 -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d test -p 5434 -U postgres "C:\minus34\GitHub\gnaf.dmp"
-"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d test -p 5434 -U postgres "C:\minus34\GitHub\admin-bdys.dmp"
+"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d psma_201602 -p 5432 -U postgres "C:\minus34\GitHub\gnaf.dmp"
+"C:\Program Files\PostgreSQL\9.5\bin\pg_restore" -Fc -d psma_201602 -p 5432 -U postgres "C:\minus34\GitHub\admin-bdys.dmp"
 
 pause
