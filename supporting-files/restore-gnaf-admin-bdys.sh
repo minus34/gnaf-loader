87354daa3cc9158a63f2af9bed090a20d44deeea
Hugh Saalmans <hugh.saalmans@gmail.com>
2016-03-01 19:48:11 +1100

    Update restore-gnaf-admin-bdys.sh

--- supporting-files/restore-gnaf-admin-bdys.sh
+++ supporting-files/restore-gnaf-admin-bdys.sh
@@ -1,2 +1,5 @@
-/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d test -p 5432 -U postgres /Users/Hugh/tmp/psma_201602/gnaf.dmp
-/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d test -p 5432 -U postgres /Users/Hugh/tmp/psma_201602/admin-bdys.dmp
+
+
+
+/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d psma_201602 -p 5432 -U postgres /Users/Hugh/tmp/psma_201602/gnaf.dmp
+/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d psma_201602 -p 5432 -U postgres /Users/Hugh/tmp/psma_201602/admin-bdys.dmp
