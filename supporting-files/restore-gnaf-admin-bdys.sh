7797098cec6caeba2f2b75683374ff683720cf47
Hugh Saalmans <hugh.saalmans@gmail.com>
2016-03-01 19:48:54 +1100

    Update restore-gnaf-admin-bdys.sh

--- supporting-files/restore-gnaf-admin-bdys.sh
+++ supporting-files/restore-gnaf-admin-bdys.sh
@@ -1,5 +1,4 @@
-
-
+psql -d psma_201602 -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
 /Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d psma_201602 -p 5432 -U postgres /Users/Hugh/tmp/psma_201602/gnaf.dmp
 /Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d psma_201602 -p 5432 -U postgres /Users/Hugh/tmp/psma_201602/admin-bdys.dmp
