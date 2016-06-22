80c998f54f477adc22a4c1e4c810506ab35ee1d9
Hugh Saalmans <hugh.saalmans@gmail.com>
2016-06-22 13:17:14 +1000

    Fixed error in script

--- supporting-files/dump-gnaf-admin-bdys.sh
+++ supporting-files/dump-gnaf-admin-bdys.sh
@@ -1,2 +1,2 @@
-/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d psma_201602 -n gnaf -p 5434 -U postgres -f /Users/Hugh/tmp/psma_201602/gnaf.dmp
-/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_restore -Fc -d psma_201602 -n admin_bdys -p 5434 -U postgres -f /Users/Hugh/tmp/psma_201602/adminbdys.dmp
+/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_dump -Fc -d psma_201605 -n gnaf -p 5432 -U postgres -f /Users/Hugh/tmp/psma_201605/gnaf.dmp
+/Applications/Postgres.app/Contents/Versions/9.5/bin/pg_dump -Fc -d psma_201605 -n admin_bdys -p 5432 -U postgres -f /Users/Hugh/tmp/psma_201605/adminbdys.dmp
