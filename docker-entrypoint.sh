e87e669422765c7efacccc608c70652f04822692
Hugh Saalmans <hugh.saalmans@gmail.com>
2016-11-20 22:07:08 +1100

    Changed python file to run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ while ! PGPASSWORD=gnaf psql -h db -U gnaf -l >/dev/null; do
 done
 
 echo "** Launching loader **"
-python load-gnaf.py --gnaf-tables-path /data/*GNAF_PipeSeparatedValue*/ --admin-bdys-path /data/ --pghost db --pgdb gnaf --pguser gnaf --pgpassword gnaf
+python load-gnaf-27.py --gnaf-tables-path /data/*GNAF_PipeSeparatedValue*/ --admin-bdys-path /data/ --pghost db --pgdb gnaf --pguser gnaf --pgpassword gnaf
