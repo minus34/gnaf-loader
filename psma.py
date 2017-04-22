6c0751368dc0dfd4bb4b20cf0830ea46f84f8581
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-04-22 14:47:13 +1000

    improved python version check at the start

--- psma.py
+++ psma.py
@@ -196,20 +196,20 @@ def check_postgis_version(pg_cur, settings, logger):
             geos_version_num = float(geos_version[:3])
     if postgis_version_num >= 2.2 and geos_version_num >= 3.5:
         settings['st_subdivide_supported'] = True
-    logger.info("")
-    logger.info("Using Postgres {0} and PostGIS {1} (with GEOS {2})"
+    logger.info("using Postgres {0} and PostGIS {1} (with GEOS {2})"
                 .format(pg_version, postgis_version, geos_version))
 
 
 def check_python_version(logger):
     # get python and psycopg2 version
-    python_version = sys.version.split("(")[0]
-    psycopg2_version = psycopg2.__version__.split("(")[0]
-    os_version = platform.system() + " " + platform.version()
+    python_version = sys.version.split("(")[0].strip()
+    psycopg2_version = psycopg2.__version__.split("(")[0].strip()
+    os_version = platform.system() + " " + platform.version().strip()
 
     logger.info("")
-    logger.info("running Python {0} on {1} with Psycopg2 {2}"
-                .format(python_version, os_version, psycopg2_version))
+    logger.info("Running Python {0} with Psycopg2 {1}"
+                .format(python_version, psycopg2_version))
+    logger.info("on {0}".format(os_version))
 
 
 def multiprocess_shapefile_load(work_list, settings, logger):
