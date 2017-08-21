e19dabcd84a2c595bfdbc585df86dcb3cde79adf
Hugh Saalmans <hugh.saalmans@gmail.com>
2017-08-22 07:35:21 +1000

    updates for Aug 2017 release

--- supporting-files/restore-gnaf-admin-bdys.bat
+++ supporting-files/restore-gnaf-admin-bdys.bat
@@ -1,7 +1,7 @@
 
 psql -d geo -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-"C:\Program Files\PostgreSQL\9.6\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\gnaf-201705.dmp"
-"C:\Program Files\PostgreSQL\9.6\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\admin-bdys-201705.dmp"
+"C:\Program Files\PostgreSQL\9.6\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\gnaf-201708.dmp"
+"C:\Program Files\PostgreSQL\9.6\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\admin-bdys-201708.dmp"
 
 pause
