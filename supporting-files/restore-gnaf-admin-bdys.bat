a50cf4df007adfb60d9d10543506d2538809c3d0
Hugh Saalmans <hugh.saalmans@iag.com.au>
2019-08-20 11:29:17 +1000

    updates for 201908 release

--- supporting-files/restore-gnaf-admin-bdys.bat
+++ supporting-files/restore-gnaf-admin-bdys.bat
@@ -1,7 +1,7 @@
 
 psql -d geo -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-"C:\Program Files\PostgreSQL\10\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\gnaf-201905.dmp"
-"C:\Program Files\PostgreSQL\10\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\admin-bdys-201905.dmp"
+"C:\Program Files\PostgreSQL\10\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\gnaf-201908.dmp"
+"C:\Program Files\PostgreSQL\10\bin\pg_restore" -Fc -d geo -p 5432 -U postgres "C:\git\minus34\admin-bdys-201908.dmp"
 
 pause
--- supporting-files/xx-export-both-gnaf-schemas.sh
+++ supporting-files/xx-export-both-gnaf-schemas.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fc -d geo -n gnaf_201905 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/gnaf-201905.dmp
-/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fc -d geo -n raw_gnaf_201905 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/raw-gnaf-201905.dmp
+/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fc -d geo -n gnaf_201908 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/gnaf-201908.dmp
+/Applications/Postgres.app/Contents/Versions/9.6/bin/pg_dump -Fc -d geo -n raw_gnaf_201908 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/raw-gnaf-201908.dmp
