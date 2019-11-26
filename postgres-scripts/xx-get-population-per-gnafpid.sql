3026a5c09ec26bde8adb4b9109674dd914213e92
Hugh Saalmans <hugh.saalmans@gmail.com>
2019-11-27 10:07:48 +1100

    testing meshblock population counts with GNAF

--- postgres-scripts/xx-get-population-per-gnafpid.sql
+++ postgres-scripts/xx-get-population-per-gnafpid.sql
@@ -16,12 +16,14 @@ CREATE TABLE testing.mb_2016_counts (
 COPY testing.mb_2016_counts (mb_2016_code, mb_category_name_2016, area_albers_sqkm, dwelling, person, state)
 FROM '/Users/hugh.saalmans/Downloads/2016 census mesh block counts.csv' WITH (FORMAT CSV, HEADER);
 
+-- gnaf_201911.address_principals
+
 
 -- Get meshblock address counts per MB -- 1 min
 WITH counts AS (
 	SELECT mb_2016_code,
 		   count(*) AS address_count
-	FROM gnaf_201608.basic_address_principals
+	FROM gnaf_201911.address_principals
 	GROUP BY mb_2016_code
 )
 UPDATE testing.mb_2016_counts AS mb
@@ -44,27 +46,35 @@ ANALYSE testing.mb_2016_counts;
 
 -- get the correct number of addresses from GNAF for each meshblock
 --    1. where address count is greater than dwelling count
-DROP TABLE IF EXISTS gnaf_201608.basic_address_principals_reduced;
-CREATE TABLE gnaf_201608.basic_address_principals_reduced AS
+DROP TABLE IF EXISTS gnaf_201911.address_principals_reduced;
+CREATE TABLE gnaf_201911.address_principals_reduced AS
 WITH adr AS (
-	SELECT mb.dwelling,
+	SELECT gnaf.gnaf_pid,
+           gnaf.mb_2016_code,
+	       mb.dwelling,
 	       mb.person,
-	       gnaf.*
-	FROM gnaf_201608.basic_address_principals as gnaf
+           mb.address_count,
+           gnaf.geom
+	FROM gnaf_201911.address_principals as gnaf
 	INNER JOIN testing.mb_2016_counts AS mb on gnaf.mb_2016_code = mb.mb_2016_code
 	WHERE mb.address_count >= mb.dwelling
 ), rows as (
     SELECT *, row_number() OVER (PARTITION BY mb_2016_code ORDER BY random()) as row_num
     FROM adr
 )
-SELECT * FROM rows
-WHERE row_num > 0
-    AND row_num <= dwelling
+SELECT gnaf_pid,
+	   mb_2016_code,
+	   address_count,
+	   dwelling,
+	   person,
+	   geom
+FROM rows
+WHERE row_num <= dwelling
 ORDER BY mb_2016_code,
          row_num
 ;
 
-ANALYSE gnaf_201608.basic_address_principals_reduced;
+ANALYSE gnaf_201911.address_principals_reduced;
 
 --    2. where address count is less than dwelling count
 
@@ -72,22 +82,28 @@ ANALYSE gnaf_201608.basic_address_principals_reduced;
 
 
 
-CREATE INDEX basic_address_principals_reduced_geom_idx ON gnaf_201608.basic_address_principals_reduced USING gist (geom);
-ALTER TABLE gnaf_201608.basic_address_principals_reduced CLUSTER ON basic_address_principals_reduced_geom_idx;
+CREATE INDEX basic_address_principals_reduced_geom_idx ON gnaf_201911.address_principals_reduced USING gist (geom);
+ALTER TABLE gnaf_201911.address_principals_reduced CLUSTER ON basic_address_principals_reduced_geom_idx;
 
-CREATE INDEX basic_address_principals_reduced_mb_2016_code_idx ON gnaf_201608.basic_address_principals_reduced USING btree(mb_2016_code);
+CREATE INDEX basic_address_principals_reduced_mb_2016_code_idx ON gnaf_201911.address_principals_reduced USING btree(mb_2016_code);
 
 
 -- 32509 MBs with less addresses than dwellings
 SELECT count(*) as cnt,
-       avg(dwelling - address_count)
+       avg(dwelling - address_count),
+	   max(dwelling - address_count)
 FROM testing.mb_2016_counts
 WHERE address_count < dwelling;
 
--- 269858 MBs with more addresses than dwellings
-SELECT count(*) FROM testing.mb_2016_counts
-WHERE address_count > dwelling;
+select *, dwelling - address_count as extra_dwellings
+FROM testing.mb_2016_counts
+WHERE address_count < dwelling
+order by extra_dwellings desc;
 
--- 15363 MBs with the same address count as dwellings
-SELECT count(*) FROM testing.mb_2016_counts
-WHERE address_count = dwelling;
+-- -- 269858 MBs with more addresses than dwellings
+-- SELECT count(*) FROM testing.mb_2016_counts
+-- WHERE address_count > dwelling;
+
+-- -- 15363 MBs with the same address count as dwellings
+-- SELECT count(*) FROM testing.mb_2016_counts
+-- WHERE address_count = dwelling;
