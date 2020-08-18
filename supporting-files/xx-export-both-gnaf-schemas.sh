5bb3cbe92e4af61297cfdcfae4900bfc85dfdfdd
Hugh Saalmans <hugh.saalmans@gmail.com>
2020-08-19 08:19:47 +1000

    changes for 202008 release

--- supporting-files/xx-export-both-gnaf-schemas.sh
+++ supporting-files/xx-export-both-gnaf-schemas.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-/Applications/Postgres.app/Contents/Versions/11/bin/pg_dump -Fc -d geo -n gnaf_202005 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/gnaf-202005.dmp
-/Applications/Postgres.app/Contents/Versions/11/bin/pg_dump -Fc -d geo -n raw_gnaf_202005 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/raw-gnaf-202005.dmp
+/Applications/Postgres.app/Contents/Versions/11/bin/pg_dump -Fc -d geo -n gnaf_202008 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/gnaf-202008.dmp
+/Applications/Postgres.app/Contents/Versions/11/bin/pg_dump -Fc -d geo -n raw_gnaf_202008 -p 5432 -U postgres -f /Users/hugh.saalmans/git/minus34/raw-gnaf-202008.dmp
