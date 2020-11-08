8668eac2e6e129b3a3c4ca91011eeb1197fc44a2
Hugh Saalmans <hugh.saalmans@gmail.com>
2020-11-08 15:46:19 +1100

    202011 updates

--- supporting-files/dump-gnaf-admin-bdys.bat
+++ supporting-files/dump-gnaf-admin-bdys.bat
@@ -1,11 +1,11 @@
-"C:\Program Files\PostgreSQL\11\bin\pg_dump" -Fc -d geo -n gnaf_202008 -p 5432 -U postgres > "C:\git\minus34\gnaf-202008.dmp"
-"C:\Program Files\PostgreSQL\11\bin\pg_dump" -Fc -d geo -n admin_bdys_202008 -p 5432 -U postgres > "C:\git\minus34\admin-bdys-202008.dmp"
+"C:\Program Files\PostgreSQL\11\bin\pg_dump" -Fc -d geo -n gnaf_202011 -p 5432 -U postgres > "C:\git\minus34\gnaf-202011.dmp"
+"C:\Program Files\PostgreSQL\11\bin\pg_dump" -Fc -d geo -n admin_bdys_202011 -p 5432 -U postgres > "C:\git\minus34\admin-bdys-202011.dmp"
 
 REM OPTIONAL - copy files to AWS S3 and allow public read access (requires awscli installed)
-REM aws --profile=default s3 cp "C:\git\minus34\gnaf-202008.dmp" s3://minus34.com/opendata/psma-202008/gnaf-202008.dmp
-REM aws --profile=default s3api put-object-acl --acl public-read --bucket minus34.com --key opendata/psma-202008/gnaf-202008.dmp
+REM aws --profile=default s3 cp "C:\git\minus34\gnaf-202011.dmp" s3://minus34.com/opendata/psma-202011/gnaf-202011.dmp
+REM aws --profile=default s3api put-object-acl --acl public-read --bucket minus34.com --key opendata/psma-202011/gnaf-202011.dmp
 
-REM aws --profile=default s3 cp "C:\git\minus34\admin-bdys-202008.dmp" s3://minus34.com/opendata/psma-202008/admin-bdys-202008.dmp
-REM aws --profile=default s3api put-object-acl --acl public-read --bucket minus34.com --key opendata/psma-202008/admin-bdys-202008.dmREM
+REM aws --profile=default s3 cp "C:\git\minus34\admin-bdys-202011.dmp" s3://minus34.com/opendata/psma-202011/admin-bdys-202011.dmp
+REM aws --profile=default s3api put-object-acl --acl public-read --bucket minus34.com --key opendata/psma-202011/admin-bdys-202011.dmREM
 
 pause
