5ac8c311e3f332237c3743e2f95066abde195d90
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-02-23 08:01:57 +1100

    updates for 202102 release

--- supporting-files/xx-run-gnaf-loader-locality-clean-and-copy-to-aws-s3.sh
+++ supporting-files/xx-run-gnaf-loader-locality-clean-and-copy-to-aws-s3.sh
@@ -4,9 +4,9 @@
 # edit these to taste - NOTE: you can't use "~" for your home folder, Postgres doesn't like it
 # ---------------------------------------------------------------------------------------------------------------------
 
-output_folder="/Users/s57405/tmp"
-gnaf_path="/Users/s57405/Downloads/FEB21_GNAF_PipeSeparatedValue_20210222101749/G-NAF"
-bdys_path="/Users/s57405/Downloads/FEB21_AdminBounds_ESRIShapefileorDBFfile/Administrative Boundaries"
+output_folder="/Users/hugh.saalmans/tmp"
+gnaf_path="/Users/hugh.saalmans/Downloads/FEB21_GNAF_PipeSeparatedValue_20210222101749/G-NAF"
+bdys_path="/Users/hugh.saalmans/Downloads/FEB21_AdminBounds_ESRIShapefileorDBFfile/Administrative Boundaries"
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -17,17 +17,16 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Run gnaf-loader and locality boundary clean
 # ---------------------------------------------------------------------------------------------------------------------
 
-python3 /Users/s57405/git/minus34/gnaf-loader/load-gnaf.py --pgport=5433 --pgdb=geo --max-processes=4 --gnaf-tables-path="${gnaf_path}" --admin-bdys-path="${bdys_path}"
-python3 /Users/s57405/git/iag_geo/psma-admin-bdys/locality-clean.py --pgport=5433 --pgdb=geo --max-processes=4 --output-path=${output_folder}
+python3 /Users/hugh.saalmans/git/minus34/gnaf-loader/load-gnaf.py --pgport=5432 --pgdb=geo --max-processes=4 --gnaf-tables-path="${gnaf_path}" --admin-bdys-path="${bdys_path}"
+python3 /Users/hugh.saalmans/git/iag_geo/psma-admin-bdys/locality-clean.py --pgport=5432 --pgdb=geo --max-processes=4 --output-path=${output_folder}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # dump postgres schemas to a local folder
 # ---------------------------------------------------------------------------------------------------------------------
 
-/Applications/Postgres.app/Contents/Versions/12/bin/pg_dump -Fc -d geo -n gnaf_202102 -p 5433 -U postgres -f "${output_folder}/gnaf-202102.dmp" --no-owner
+/Applications/Postgres.app/Contents/Versions/12/bin/pg_dump -Fc -d geo -n gnaf_202102 -p 5432 -U postgres -f "${output_folder}/gnaf-202102.dmp" --no-owner
 echo "GNAF schema exported to dump file"
-
-/Applications/Postgres.app/Contents/Versions/12/bin/pg_dump -Fc -d geo -n admin_bdys_202102 -p 5433 -U postgres -f "${output_folder}/admin-bdys-202102.dmp" --no-owner
+/Applications/Postgres.app/Contents/Versions/12/bin/pg_dump -Fc -d geo -n admin_bdys_202102 -p 5432 -U postgres -f "${output_folder}/admin-bdys-202102.dmp" --no-owner
 echo "Admin Bdys schema exported to dump file"
 
 # ---------------------------------------------------------------------------------------------------------------------
