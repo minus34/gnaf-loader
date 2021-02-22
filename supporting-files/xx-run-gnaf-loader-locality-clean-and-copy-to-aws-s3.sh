8f8827a3ebe734e80c7096acc9291280f66fe508
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-02-22 17:42:54 +1100

    updates for 202102 release

--- supporting-files/xx-run-gnaf-loader-locality-clean-and-copy-to-aws-s3.sh
+++ supporting-files/xx-run-gnaf-loader-locality-clean-and-copy-to-aws-s3.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # set these to taste - NOTE: you can't use "~" for your home folder
-output_folder="/Users/hugh.saalmans/tmp"
-gnaf_path="/Users/hugh.saalmans/Downloads/nov20_gnaf_pipeseparatedvalue/G-NAF"
-bdys_path="/Users/hugh.saalmans/Downloads/nov20_adminbounds_esrishapefileordbffile/Administrative Boundaries"
+output_folder="/Users/s57405/tmp"
+gnaf_path="/Users/s57405/Downloads/nov20_gnaf_pipeseparatedvalue/G-NAF"
+bdys_path="/Users/s57405/Downloads/feb21_adminbounds_esrishapefileordbffile/Administrative Boundaries"
 
 # run gnaf-loader
-python3 /Users/hugh.saalmans/git/minus34/gnaf-loader/load-gnaf.py --pgdb=geo --max-processes=4 --gnaf-tables-path="${gnaf_path}" --admin-bdys-path="${bdys_path}"
+python3 /Users/s57405/git/minus34/gnaf-loader/load-gnaf.py --pgdb=geo --max-processes=4 --gnaf-tables-path="${gnaf_path}" --admin-bdys-path="${bdys_path}"
 
 # run locality clean
-python3 /Users/hugh.saalmans/git/iag_geo/psma-admin-bdys/locality-clean.py --output-path=${output_folder}
+python3 /Users/s57405/git/iag_geo/psma-admin-bdys/locality-clean.py --output-path=${output_folder}
 
 # dump postgres schemas to a local folder
 /Applications/Postgres.app/Contents/Versions/12/bin/pg_dump -Fc -d geo -n gnaf_202102 -p 5432 -U postgres -f "${output_folder}/gnaf-202102.dmp" --no-owner
@@ -30,4 +30,5 @@ for f in *-202102.dmp;
   done
 
 # OPTIONAL - create parquet version of GNAF and Admin Bdys and upload to AWS S3
-.  01_setup_pyspark_3.sh to have been run previously
+. ../spark/01_setup_pyspark_3.sh
+python ../spark/02_export_gnaf_and_admin_bdys_to_s3.py
