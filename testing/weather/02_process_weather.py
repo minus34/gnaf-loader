c80b787a63cedbfcadf6cb435d11202e8198b469
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-03-01 11:53:18 +1100

    obs multiprocessing download working

--- testing/weather/02_process_weather.py
+++ testing/weather/02_process_weather.py
@@ -2,12 +2,15 @@
 # ... and saves them to a CSV
 
 import csv
+import logging
+import multiprocessing
 import os
 # import pandas
 # import matplotlib.pyplot as plt
 import requests
 
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 # where to save the files
 output_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")
@@ -18,43 +21,109 @@ states = ["ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"]
 # urls for each state's weather observations
 base_url = "http://www.bom.gov.au/{0}/observations/{0}all.shtml"
 
-obs_urls = list()
 
-for state in states:
-    # get URL for web page to scrape
-    input_url = base_url.format(state.lower())
+def main():
+    start_time = datetime.now()
 
-    # load and parse web page
-    r = requests.get(input_url)
-    soup = BeautifulSoup(r.content, features="html.parser")
+    obs_urls = list()
 
-    # get all links
-    links = soup.find_all('a', href=True)
+    for state in states:
+        # get URL for web page to scrape
+        input_url = base_url.format(state.lower())
 
-    for link in links:
-        url = link['href']
+        # load and parse web page
+        r = requests.get(input_url)
+        soup = BeautifulSoup(r.content, features="html.parser")
 
-        if "/products/" in url:
+        # get all links
+        links = soup.find_all('a', href=True)
 
-            # change URL to get JSON file of weather obs
-            obs_url = url.replace("/products/", "http://www.bom.gov.au/fwo/").replace(".shtml", ".json")
-            # print(obs_url)
+        for link in links:
+            url = link['href']
 
-            obs_urls.append(obs_url)
+            if "/products/" in url:
 
-with open(os.path.join(output_path, 'weather_observations_urls.txt'), 'w', newline='') as output_file:
-    output_file.write("\n".join(obs_urls))
+                # change URL to get JSON file of weather obs
+                obs_url = url.replace("/products/", "http://www.bom.gov.au/fwo/").replace(".shtml", ".json")
+                # logger.info(obs_url)
 
-# download each obs file    
-i = 1
-num_urls = len(obs_urls)
+                obs_urls.append(obs_url)
 
-for url in obs_urls:
+    with open(os.path.join(output_path, 'weather_observations_urls.txt'), 'w', newline='') as output_file:
+        output_file.write("\n".join(obs_urls))
+
+    logger.info("Got obs file list : {}".format(datetime.now() - start_time))
+    start_time = datetime.now()
+
+    # download each obs file using multiprocessing
+    pool = multiprocessing.Pool(processes=12)
+
+    num_jobs = len(obs_urls)
+
+    results = pool.imap_unordered(run_multiprocessing, obs_urls)
+
+    pool.close()
+    pool.join()
+
+    result_list = list(results)
+    num_results = len(result_list)
+
+    if num_jobs > num_results:
+        logger.warning("\t- A MULTIPROCESSING PROCESS FAILED WITHOUT AN ERROR\nACTION: Check the record counts")
+
+    for result in result_list:
+        if result != "SUCCESS":
+            logger.warning(result)
+
+    logger.info("Downloaded all obs files : {}".format(datetime.now() - start_time))
+    # start_time = datetime.now()
+
+    return True
+
+
+def run_multiprocessing(url):
     file_path = os.path.join(output_path, "obs", url.split("/")[-1])
 
-    with open(file_path, 'w', newline='') as output_file:
-        output_file.write(requests.get(url).text)
+    try:
+        with open(file_path, 'w', newline='') as output_file:
+            output_file.write(requests.get(url).text)
 
-    print("Saved {} : {} of {}".format(file_path, i, num_urls))
+        result = "SUCCESS"
+        # print("Saved {}".format(file_path))
+    except Exception as ex:
+        result = "\t- FAILED! : {} : {}".format(file_path, ex)
+        # print(result)
 
-    i += 1
+    return result
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger()
+
+    # set logger
+    log_file = os.path.abspath(__file__).replace(".py", ".log")
+    logging.basicConfig(filename=log_file, level=logging.DEBUG, format="%(asctime)s %(message)s",
+                        datefmt="%m/%d/%Y %I:%M:%S %p")
+
+    # setup logger to write to screen as well as writing to log file
+    # define a Handler which writes INFO messages or higher to the sys.stderr
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+
+    logger.info("")
+    logger.info("Start weather obs download")
+    # psma.check_python_version(logger)
+
+    if main():
+        logger.info("Finished successfully!")
+    else:
+        logger.fatal("Something bad happened!")
+
+    logger.info("")
+    logger.info("-------------------------------------------------------------------------------")
