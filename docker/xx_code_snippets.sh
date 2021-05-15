08c4fa90d7a07297ec6ce970c1f56ef1ee8598ad
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-05-15 14:46:12 +1000

    working on new Dockerfile

--- docker/xx_code_snippets.sh
+++ docker/xx_code_snippets.sh
@@ -3,4 +3,8 @@
 
 
 
-docker run --name=gnaf-loader -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password -publish=5433:5432
+docker build --build-arg PGUSER=postgres --build-arg PGPASSWORD=password --tag minus34/gnafloader:202105
+
+#--no-cache
+
+docker run --name=gnaf-loader -e PGUSER=postgres -e PGPASSWORD=password -publish=5433:5432 minus34/gnafloader:202105
