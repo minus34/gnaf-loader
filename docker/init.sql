c6d0a7845edfc1233f1f939e6927fcb60655128b
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-05-19 20:52:29 +1000

    changed source image

--- docker/init.sql
+++
@@ -1,6 +0,0 @@
-
---CREATE DATABASE geo;
-
-CREATE EXTENSION postgis;
-
-SELECT PostGIS_Full_Version();
--- docker/xx_code_snippets.sh
+++ docker/xx_code_snippets.sh
@@ -1,19 +1,6 @@
 
+# build gnaf loader image
+docker build --tag minus34/gnafloader:latest --tag minus34/gnafloader:202105 .
 
-
-
-
-docker build --build-arg POSTGRES_DB=geo --build-arg POSTGRES_USER=postgres --build-arg POSTGRES_PASSWORD=password --tag minus34/gnafloader:202105 .
-
-#--no-cache
-
-docker run --name=gnafloader -e POSTGRES_DB=geo -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=password --publish=5433:5432 minus34/gnafloader:202105 .
-
-
-
-
-docker build --tag minus34/gnafloader --tag minus34/gnafloader:latest --tag minus34/gnafloader:202105 .
-
+# run gnaf loader container
 docker run --name=gnafloader --publish=5433:5432 minus34/gnafloader:latest
-
-
