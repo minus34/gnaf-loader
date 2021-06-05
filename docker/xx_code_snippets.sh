c5e5dd6c36f6b30f5b78ddb9ec54fbcb175976b4
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-06-05 14:41:58 +1000

    added cd statement

--- docker/xx_code_snippets.sh
+++ docker/xx_code_snippets.sh
@@ -1,6 +1,8 @@
 
+cd /Users/$(whoami)/git/minus34/gnaf-loader/docker
+
 # build gnaf loader image
-docker build --tag minus34/gnafloader:latest --tag minus34/gnafloader:202105 .
+docker build --squash --tag minus34/gnafloader:latest --tag minus34/gnafloader:202105 .
 
 # run gnaf loader container
 docker run --name=gnafloader --publish=5433:5432 minus34/gnafloader:latest
