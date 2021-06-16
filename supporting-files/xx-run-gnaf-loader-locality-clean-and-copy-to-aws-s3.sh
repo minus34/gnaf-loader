ce6eee457cd43cb2dfb639885e9b85fa5f722944
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-06-16 15:42:17 +1000

    improved upload to S3

--- supporting-files/xx-run-gnaf-loader-locality-clean-and-copy-to-aws-s3.sh
+++ supporting-files/xx-run-gnaf-loader-locality-clean-and-copy-to-aws-s3.sh
@@ -3,6 +3,9 @@
 # need a Python 3.7+ environment with Psycopg2
 conda activate minus34
 
+# get the directory this script is running from
+GNAF_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # ---------------------------------------------------------------------------------------------------------------------
 # edit these to taste - NOTE: you can't use "~" for your home folder, Postgres doesn't like it
 # ---------------------------------------------------------------------------------------------------------------------
@@ -12,14 +15,9 @@ OUTPUT_FOLDER="/Users/$(whoami)/tmp/geoscape_202105"
 GNAF_PATH="/Users/$(whoami)/Downloads/G-NAF_MAY21_AUSTRALIA_GDA94"
 BDYS_PATH="/Users/$(whoami)/Downloads/MAY21 AdminBounds ESRIShapefileorDBFfile"
 
-# ---------------------------------------------------------------------------------------------------------------------
-
-# get the directory this script is running from
-GNAF_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-# ---------------------------------------------------------------------------------------------------------------------
-# Run gnaf-loader and locality boundary clean
-# ---------------------------------------------------------------------------------------------------------------------
+echo ---------------------------------------------------------------------------------------------------------------------
+echo Run gnaf-loader and locality boundary clean
+echo ---------------------------------------------------------------------------------------------------------------------
 
 python3 /Users/$(whoami)/git/minus34/gnaf-loader/load-gnaf.py --pgport=5432 --pgdb=geo --max-processes=6 --gnaf-tables-path="${GNAF_PATH}" --admin-bdys-path="${BDYS_PATH}"
 python3 /Users/$(whoami)/git/iag_geo/psma-admin-bdys/locality-clean.py --pgport=5432 --pgdb=geo --max-processes=6 --output-path=${OUTPUT_FOLDER}
@@ -41,16 +39,9 @@ echo ---------------------------------------------------------------------------
 
 aws --profile=${AWS_PROFILE} s3 sync ${OUTPUT_FOLDER} s3://minus34.com/opendata/geoscape-202105 --exclude "*" --include "*.dmp" --acl public-read
 
-#for f in *-202105.dmp;
-#  do
-#    aws --profile=${AWS_PROFILE} s3 cp --storage-class REDUCED_REDUNDANCY "./${f}" s3://minus34.com/opendata/geoscape-202105/${f};
-#    aws --profile=${AWS_PROFILE} s3api put-object-acl --acl public-read --bucket minus34.com --key opendata/geoscape-202105/${f}
-#    echo "${f} uploaded to AWS S3"
-#  done
-
-## ---------------------------------------------------------------------------------------------------------------------
-## build gnafloader docker image
-## ---------------------------------------------------------------------------------------------------------------------
+#echo ---------------------------------------------------------------------------------------------------------------------
+#echo build gnafloader docker image
+#echo ---------------------------------------------------------------------------------------------------------------------
 #
 #cd /Users/$(whoami)/git/minus34/gnaf-loader/docker
 #docker build --tag minus34/gnafloader:latest --tag minus34/gnafloader:202105 .
