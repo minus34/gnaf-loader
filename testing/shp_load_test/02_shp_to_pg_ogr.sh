372952530fe51220c792480ad1daba15937116d0
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-09-03 12:48:43 +1000

    working on ogr vs geopandas vs shp2pgsql test - ogr working

--- testing/shp_load_test/02_shp_to_pg_ogr.sh
+++ testing/shp_load_test/02_shp_to_pg_ogr.sh
@@ -13,18 +13,25 @@ SECONDS=0*
 # create an array of state names
 declare -a STATES=("ACT" "NSW" "NT" "OT" "QLD" "SA" "TAS" "VIC" "WA")
 
-for STATE in "${STATES[@]}"
+for i in $(seq 1 5);
 do
-  echo " Importing ${STATE}"
-  SHP_PATH="/Users/s57405/Downloads/AUG21_Admin_Boundaries_ESRIShapefileorDBFfile/Localities_AUG21_GDA94_SHP/Localities/Localities AUGUST 2021/Standard/${STATE}_localities.shp"
+  echo " ROUND ${i} OF 5"
 
-  if [[ ${STATE} == "ACT" ]]
-  then
-    ogr2ogr -f "PostgreSQL" -overwrite -nln testing.locality PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "${SHP_PATH}"
-  else
-    ogr2ogr -f "PostgreSQL" -append -update -nln testing.locality PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "${SHP_PATH}"
-  fi
+  for STATE in "${STATES[@]}"
+  do
+    SHP_PATH="/Users/s57405/Downloads/AUG21_Admin_Boundaries_ESRIShapefileorDBFfile/Localities_AUG21_GDA94_SHP/Localities/Localities AUGUST 2021/Standard/${STATE}_localities.shp"
 
+    if [[ ${STATE} == "ACT" ]]
+    then
+      echo -n "  - importing ${STATE}"
+      ogr2ogr -f "PostgreSQL" -overwrite -nln testing.locality PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "${SHP_PATH}"
+    else
+      echo -n ", ${STATE}"
+      ogr2ogr -f "PostgreSQL" -append -update -nln testing.locality PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "${SHP_PATH}"
+    fi
+  done
+
+  echo ""
 done
 
 duration=$SECONDS
