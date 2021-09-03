5293e5baeffb5eba8facaafd79352241c94d8710
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-09-03 12:14:08 +1000

    working on ogr vs geopandas vs shp2pgsql test

--- testing/shp_load_test/02_shp_to_pg_ogr.sh
+++ testing/shp_load_test/02_shp_to_pg_ogr.sh
@@ -13,17 +13,19 @@ SECONDS=0*
 STATE="ACT"
 SHP_PATH="/Users/s57405/Downloads/AUG21_Admin_Boundaries_ESRIShapefileorDBFfile/Localities_AUG21_GDA94_SHP/Localities/Localities AUGUST 2021/Standard/${STATE}_localities.shp"
 
-
-ogr2ogr -f "PostgreSQL" PG:"host=localhost port=5432 dbname=geo user=postgres password=password" ${SHP_PATH}
+ogr2ogr -overwrite -f "PostgreSQL" -nln locality -lco SCHEMA=testing PG:"host=localhost port=5432 dbname=geo user=postgres password=password" "${SHP_PATH}"
 
 
 duration=$SECONDS
 
 echo "-------------------------------------------------------------------------"
 echo " End time : $(date)"
-echo " Test took $(duration) seconds"
+echo " Test took ${duration} seconds"
 echo "----------------------------------------------------------------------------------------------------------------"
 
 
 
 
+#ogr2ogr -f PostgreSQL PG:dbname=destination_db   input_polygons.shp \
+#        -nln destination_table  -update  -append  -t_srs "EPSG:4326" \
+#        -sql "SELECT *, row_to_json(input_polygons) as attributes from input_polygons"
