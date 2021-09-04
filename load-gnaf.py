c584fde001640c37056b2ea89ca5c060ec9250c1
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-09-04 14:02:25 +1000

    cleaned up pk & fk process with deduplicated authority tables

--- load-gnaf.py
+++ load-gnaf.py
@@ -100,13 +100,13 @@ def main():
     drop_tables_and_vacuum_db(pg_cur)
     create_raw_gnaf_tables(pg_cur)
     populate_raw_gnaf()
+    clean_authority_files(pg_cur, settings.raw_gnaf_schema, False)
     index_raw_gnaf()
     if settings.primary_foreign_keys:
-        create_primary_foreign_keys(False)
+        create_primary_foreign_keys()
     else:
         logger.info("\t- Step 6 of 7 : primary & foreign keys NOT created")
     analyse_raw_gnaf_tables(pg_cur)
-    clean_authority_files(pg_cur, settings.raw_gnaf_schema, True)
     # set postgres search path back to the default
     pg_cur.execute("SET search_path = public, pg_catalog")
     logger.info("Part 2 of 6 : Raw GNAF loaded! : {0}".format(datetime.now() - start_time))
@@ -116,7 +116,7 @@ def main():
     # start_time = datetime.now()
     # logger.info("Part 3 of 6 : Start raw admin boundary load : {0}".format(start_time))
     # load_raw_admin_boundaries(pg_cur)
-    # clean_authority_files(pg_cur, settings.raw_admin_bdys_schema, False)
+    # clean_authority_files(pg_cur, settings.raw_admin_bdys_schema, True)
     # prep_admin_bdys(pg_cur)
     # create_admin_bdys_for_analysis()
     # logger.info("Part 3 of 6 : Raw admin boundaries loaded! : {0}".format(datetime.now() - start_time))
@@ -263,15 +263,10 @@ def index_raw_gnaf():
 
 
 # create raw gnaf primary & foreign keys (for data integrity) using multiprocessing
-def create_primary_foreign_keys(aut=False):
+def create_primary_foreign_keys():
     start_time = datetime.now()
 
-    # FK & PK for authority tables or not?
-    if aut:
-        key_sql = geoscape.open_sql_file("01-06c-raw-gnaf-aut-create-primary-foreign-keys.sql")
-    else:
-        key_sql = geoscape.open_sql_file("01-06-raw-gnaf-create-primary-foreign-keys.sql")
-
+    key_sql = geoscape.open_sql_file("01-06-raw-gnaf-create-primary-foreign-keys.sql")
     key_sql_list = key_sql.split("--")
     sql_list = []
 
@@ -285,10 +280,7 @@ def create_primary_foreign_keys(aut=False):
     # run queries in separate processes
     geoscape.multiprocess_list("sql", sql_list, logger)
 
-    if aut:
-        logger.info("\t\t - authority table primary & foreign keys created : {0}".format(datetime.now() - start_time))
-    else:
-        logger.info("\t- Step 6 of 7 : primary & foreign keys created : {0}".format(datetime.now() - start_time))
+    logger.info("\t- Step 6 of 7 : primary & foreign keys created : {0}".format(datetime.now() - start_time))
 
 
 # analyse raw GNAF tables that have not stats - need actual row counts for QA at the end
@@ -486,12 +478,6 @@ def clean_authority_files(pg_cur, schema_name, create_indexes=False):
         # This is required due to complexities introduced by mix of authority
         #   and non-authority table admin bdy layers in the Aug 2021 release
         if create_indexes:
-            # create PKs and FKs on GNAF authority tables
-            if settings.primary_foreign_keys:
-                create_primary_foreign_keys(True)
-        else:
-            # sort out PKs on admin bdy authority tables
-
             # drop primary key on gid field
             try:
                 pg_cur.execute("ALTER TABLE ONLY {0}.{1} DROP CONSTRAINT {1}_pkey"
--- postgres-scripts/01-06-raw-gnaf-create-primary-foreign-keys.sql
+++ postgres-scripts/01-06-raw-gnaf-create-primary-foreign-keys.sql
@@ -8,6 +8,14 @@ ALTER TABLE ONLY address_alias
 
 
 --
+-- Name: address_alias_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY address_alias_type_aut
+    ADD CONSTRAINT address_alias_type_aut_pk PRIMARY KEY (code);
+
+
+--
 -- Name: address_default_geocode_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
@@ -64,11 +72,75 @@ ALTER TABLE ONLY address_site
 
 
 --
+-- Name: address_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY address_type_aut
+    ADD CONSTRAINT address_type_aut_pk PRIMARY KEY (code);
+
+
+--
+-- Name: flat_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY flat_type_aut
+    ADD CONSTRAINT flat_type_aut_pk PRIMARY KEY (code);
+
+
+--
+-- Name: geocode_reliability_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY geocode_reliability_aut
+    ADD CONSTRAINT geocode_reliability_aut_pk PRIMARY KEY (code);
+
+
+--
+-- Name: geocode_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY geocode_type_aut
+    ADD CONSTRAINT geocode_type_aut_pk PRIMARY KEY (code);
+
+
+--
+-- Name: geocoded_level_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY geocoded_level_type_aut
+    ADD CONSTRAINT geocoded_level_type_aut_pk PRIMARY KEY (code);
+
+
+--
+-- Name: level_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY level_type_aut
+    ADD CONSTRAINT level_type_aut_pk PRIMARY KEY (code);
+
+
+--
 -- Name: locality_alias_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
 ALTER TABLE ONLY locality_alias
     ADD CONSTRAINT locality_alias_pk PRIMARY KEY (locality_alias_pid);
+
+
+--
+-- Name: locality_alias_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY locality_alias_type_aut
+    ADD CONSTRAINT locality_alias_type_aut_pk PRIMARY KEY (code);
+
+
+--
+-- Name: locality_class_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY locality_class_aut
+    ADD CONSTRAINT locality_class_aut_pk PRIMARY KEY (code);
 
 
 --
@@ -120,11 +192,27 @@ ALTER TABLE ONLY mb_2016
 
 
 --
+-- Name: mb_match_code_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY mb_match_code_aut
+    ADD CONSTRAINT mb_match_code_aut_pk PRIMARY KEY (code);
+
+
+--
 -- Name: primary_secondary_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
 ALTER TABLE ONLY primary_secondary
     ADD CONSTRAINT primary_secondary_pk PRIMARY KEY (primary_secondary_pid);
+
+
+--
+-- Name: ps_join_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY ps_join_type_aut
+    ADD CONSTRAINT ps_join_type_aut_pk PRIMARY KEY (code);
 
 
 --
@@ -136,11 +224,27 @@ ALTER TABLE ONLY state
 
 
 --
+-- Name: street_class_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY street_class_aut
+    ADD CONSTRAINT street_class_aut_pk PRIMARY KEY (code);
+
+
+--
 -- Name: street_locality_alias_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace: 
 --
 
 ALTER TABLE ONLY street_locality_alias
     ADD CONSTRAINT street_locality_alias_pk PRIMARY KEY (street_locality_alias_pid);
+
+
+--
+-- Name: street_locality_alias_type__pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY street_locality_alias_type_aut
+    ADD CONSTRAINT street_locality_alias_type__pk PRIMARY KEY (code);
 
 
 --
@@ -160,6 +264,22 @@ ALTER TABLE ONLY street_locality_point
 
 
 --
+-- Name: street_suffix_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY street_suffix_aut
+    ADD CONSTRAINT street_suffix_aut_pk PRIMARY KEY (code);
+
+
+--
+-- Name: street_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY street_type_aut
+    ADD CONSTRAINT street_type_aut_pk PRIMARY KEY (code);
+
+
+--
 -- Name: address_feature_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
 --
 
@@ -168,11 +288,27 @@ ALTER TABLE ONLY address_feature
 
 
 --
+-- Name: address_change_type_aut_pk; Type: CONSTRAINT; Schema: public; Owner: postgres; Tablespace:
+--
+
+ALTER TABLE ONLY address_change_type_aut
+    ADD CONSTRAINT address_change_type_aut_pk PRIMARY KEY (code);
+
+
+--
 -- Name: address_alias_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY address_alias
     ADD CONSTRAINT address_alias_fk1 FOREIGN KEY (alias_pid) REFERENCES address_detail(address_detail_pid);
+
+
+--
+-- Name: address_alias_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_alias
+    ADD CONSTRAINT address_alias_fk2 FOREIGN KEY (alias_type_code) REFERENCES address_alias_type_aut(code);
 
 
 --
@@ -192,11 +328,43 @@ ALTER TABLE ONLY address_default_geocode
 
 
 --
+-- Name: address_default_geocode_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_default_geocode
+    ADD CONSTRAINT address_default_geocode_fk2 FOREIGN KEY (geocode_type_code) REFERENCES geocode_type_aut(code);
+
+
+--
 -- Name: address_detail_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY address_detail
     ADD CONSTRAINT address_detail_fk1 FOREIGN KEY (address_site_pid) REFERENCES address_site(address_site_pid);
+
+
+--
+-- Name: address_detail_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_detail
+    ADD CONSTRAINT address_detail_fk2 FOREIGN KEY (flat_type_code) REFERENCES flat_type_aut(code);
+
+
+--
+-- Name: address_detail_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_detail
+    ADD CONSTRAINT address_detail_fk3 FOREIGN KEY (level_geocoded_code) REFERENCES geocoded_level_type_aut(code);
+
+
+--
+-- Name: address_detail_fk4; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_detail
+    ADD CONSTRAINT address_detail_fk4 FOREIGN KEY (level_type_code) REFERENCES level_type_aut(code);
 
 
 --
@@ -232,6 +400,14 @@ ALTER TABLE ONLY address_mesh_block_2011
 
 
 --
+-- Name: address_mesh_block_2011_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_mesh_block_2011
+    ADD CONSTRAINT address_mesh_block_2011_fk3 FOREIGN KEY (mb_match_code) REFERENCES mb_match_code_aut(code);
+
+
+--
 -- Name: address_mesh_block_2021_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -245,6 +421,14 @@ ALTER TABLE ONLY address_mesh_block_2021
 
 ALTER TABLE ONLY address_mesh_block_2021
     ADD CONSTRAINT address_mesh_block_2021_fk2 FOREIGN KEY (mb_2021_pid) REFERENCES mb_2021(mb_2021_pid);
+
+
+--
+-- Name: address_mesh_block_2021_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_mesh_block_2021
+    ADD CONSTRAINT address_mesh_block_2021_fk3 FOREIGN KEY (mb_match_code) REFERENCES mb_match_code_aut(code);
 
 
 --
@@ -264,6 +448,22 @@ ALTER TABLE ONLY address_mesh_block_2016
 
 
 --
+-- Name: address_mesh_block_2016_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_mesh_block_2016
+    ADD CONSTRAINT address_mesh_block_2016_fk3 FOREIGN KEY (mb_match_code) REFERENCES mb_match_code_aut(code);
+
+
+--
+-- Name: address_site_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_site
+    ADD CONSTRAINT address_site_fk1 FOREIGN KEY (address_type) REFERENCES address_type_aut(code);
+
+
+--
 -- Name: address_site_geocode_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -272,11 +472,51 @@ ALTER TABLE ONLY address_site_geocode
 
 
 --
+-- Name: address_site_geocode_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_site_geocode
+    ADD CONSTRAINT address_site_geocode_fk2 FOREIGN KEY (geocode_type_code) REFERENCES geocode_type_aut(code);
+
+
+--
+-- Name: address_site_geocode_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_site_geocode
+    ADD CONSTRAINT address_site_geocode_fk3 FOREIGN KEY (reliability_code) REFERENCES geocode_reliability_aut(code);
+
+
+--
+-- Name: locality_alias_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY locality_alias
+    ADD CONSTRAINT locality_alias_fk1 FOREIGN KEY (alias_type_code) REFERENCES locality_alias_type_aut(code);
+
+
+--
 -- Name: locality_alias_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY locality_alias
     ADD CONSTRAINT locality_alias_fk2 FOREIGN KEY (locality_pid) REFERENCES locality(locality_pid);
+
+
+--
+-- Name: locality_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY locality
+    ADD CONSTRAINT locality_fk1 FOREIGN KEY (gnaf_reliability_code) REFERENCES geocode_reliability_aut(code);
+
+
+--
+-- Name: locality_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY locality
+    ADD CONSTRAINT locality_fk2 FOREIGN KEY (locality_class_code) REFERENCES locality_class_aut(code);
 
 
 --
@@ -320,11 +560,27 @@ ALTER TABLE ONLY primary_secondary
 
 
 --
+-- Name: primary_secondary_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY primary_secondary
+    ADD CONSTRAINT primary_secondary_fk2 FOREIGN KEY (ps_join_type_code) REFERENCES ps_join_type_aut(code);
+
+
+--
 -- Name: primary_secondary_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY primary_secondary
     ADD CONSTRAINT primary_secondary_fk3 FOREIGN KEY (secondary_pid) REFERENCES address_detail(address_detail_pid);
+
+
+--
+-- Name: street_locality_alias_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY street_locality_alias
+    ADD CONSTRAINT street_locality_alias_fk1 FOREIGN KEY (alias_type_code) REFERENCES street_locality_alias_type_aut(code);
 
 
 --
@@ -336,11 +592,59 @@ ALTER TABLE ONLY street_locality_alias
 
 
 --
+-- Name: street_locality_alias_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY street_locality_alias
+    ADD CONSTRAINT street_locality_alias_fk3 FOREIGN KEY (street_suffix_code) REFERENCES street_suffix_aut(code);
+
+
+--
+-- Name: street_locality_alias_fk4; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY street_locality_alias
+    ADD CONSTRAINT street_locality_alias_fk4 FOREIGN KEY (street_type_code) REFERENCES street_type_aut(code);
+
+
+--
+-- Name: street_locality_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY street_locality
+    ADD CONSTRAINT street_locality_fk1 FOREIGN KEY (gnaf_reliability_code) REFERENCES geocode_reliability_aut(code);
+
+
+--
 -- Name: street_locality_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY street_locality
     ADD CONSTRAINT street_locality_fk2 FOREIGN KEY (locality_pid) REFERENCES locality(locality_pid);
+
+
+--
+-- Name: street_locality_fk3; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY street_locality
+    ADD CONSTRAINT street_locality_fk3 FOREIGN KEY (street_class_code) REFERENCES street_class_aut(code);
+
+
+--
+-- Name: street_locality_fk4; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY street_locality
+    ADD CONSTRAINT street_locality_fk4 FOREIGN KEY (street_suffix_code) REFERENCES street_suffix_aut(code);
+
+
+--
+-- Name: street_locality_fk5; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY street_locality
+    ADD CONSTRAINT street_locality_fk5 FOREIGN KEY (street_type_code) REFERENCES street_type_aut(code);
 
 
 --
@@ -357,3 +661,11 @@ ALTER TABLE ONLY street_locality_point
 
 ALTER TABLE ONLY address_feature
     ADD CONSTRAINT address_feature_fk1 FOREIGN KEY (address_detail_pid) REFERENCES address_detail(address_detail_pid);
+
+
+--
+-- Name: address_feature_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY address_feature
+    ADD CONSTRAINT address_feature_fk2 FOREIGN KEY (address_change_type_code) REFERENCES address_change_type_aut(code);
