c0b5f4485ae629d1dbbc45860d8ce41f3d98c1dc
Hugh Saalmans <hugh.saalmans@iag.com.au>
2021-09-09 14:43:37 +1000

    removed geoscape version suffixes from schema names

--- postgres-scripts/02-02d-prep-census-2021-bdys-tables.sql
+++ postgres-scripts/02-02d-prep-census-2021-bdys-tables.sql
@@ -3,8 +3,8 @@
 -- ABS census 2021 - meshblocks
 --------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS admin_bdys_202108.abs_2021_mb CASCADE;
-CREATE TABLE admin_bdys_202108.abs_2021_mb AS
+DROP TABLE IF EXISTS admin_bdys.abs_2021_mb CASCADE;
+CREATE TABLE admin_bdys.abs_2021_mb AS
 SELECT tab.gid,
        mb_21ppid,
        tab.dt_create,
@@ -29,20 +29,20 @@ SELECT tab.gid,
        mb21_pop,
        loci21_uri::text,
        geom
-  FROM raw_admin_bdys_202108.aus_mb_2021 AS tab
-  INNER JOIN raw_admin_bdys_202108.aus_state AS ste ON tab.state_pid = ste.state_pid;
+  FROM raw_admin_bdys.aus_mb_2021 AS tab
+  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
 
-ALTER TABLE admin_bdys_202108.abs_2021_mb ADD CONSTRAINT abs_2021_mb_pk PRIMARY KEY (gid);
-CREATE INDEX abs_2021_mb_geom_idx ON admin_bdys_202108.abs_2021_mb USING gist(geom);
-ALTER TABLE admin_bdys_202108.abs_2021_mb CLUSTER ON abs_2021_mb_geom_idx;
+ALTER TABLE admin_bdys.abs_2021_mb ADD CONSTRAINT abs_2021_mb_pk PRIMARY KEY (gid);
+CREATE INDEX abs_2021_mb_geom_idx ON admin_bdys.abs_2021_mb USING gist(geom);
+ALTER TABLE admin_bdys.abs_2021_mb CLUSTER ON abs_2021_mb_geom_idx;
 
 
 -- # ---------------------------------------------------------------------------------
 -- ABS census 2021 - statistical area 1's
 --------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS admin_bdys_202108.abs_2021_sa1 CASCADE;
-CREATE TABLE admin_bdys_202108.abs_2021_sa1 AS
+DROP TABLE IF EXISTS admin_bdys.abs_2021_sa1 CASCADE;
+CREATE TABLE admin_bdys.abs_2021_sa1 AS
 SELECT tab.gid,
        sa1_21ppid,
        tab.dt_create,
@@ -63,20 +63,20 @@ SELECT tab.gid,
        area_sqm,
        loci21_uri::text,
        geom
-  FROM raw_admin_bdys_202108.aus_sa1_2021 AS tab
-  INNER JOIN raw_admin_bdys_202108.aus_state AS ste ON tab.state_pid = ste.state_pid;
+  FROM raw_admin_bdys.aus_sa1_2021 AS tab
+  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
 
-ALTER TABLE admin_bdys_202108.abs_2021_sa1 ADD CONSTRAINT abs_2021_sa1_pk PRIMARY KEY (gid);
-CREATE INDEX abs_2021_sa1_geom_idx ON admin_bdys_202108.abs_2021_sa1 USING gist(geom);
-ALTER TABLE admin_bdys_202108.abs_2021_sa1 CLUSTER ON abs_2021_sa1_geom_idx;
+ALTER TABLE admin_bdys.abs_2021_sa1 ADD CONSTRAINT abs_2021_sa1_pk PRIMARY KEY (gid);
+CREATE INDEX abs_2021_sa1_geom_idx ON admin_bdys.abs_2021_sa1 USING gist(geom);
+ALTER TABLE admin_bdys.abs_2021_sa1 CLUSTER ON abs_2021_sa1_geom_idx;
 
 
 -- # ---------------------------------------------------------------------------------
 -- ABS census 2021 - statistical area 2's
 --------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS admin_bdys_202108.abs_2021_sa2 CASCADE;
-CREATE TABLE admin_bdys_202108.abs_2021_sa2 AS
+DROP TABLE IF EXISTS admin_bdys.abs_2021_sa2 CASCADE;
+CREATE TABLE admin_bdys.abs_2021_sa2 AS
 SELECT tab.gid,
        sa2_21ppid,
        tab.dt_create,
@@ -96,20 +96,20 @@ SELECT tab.gid,
        area_sqm,
        loci21_uri::text,
        geom
-  FROM raw_admin_bdys_202108.aus_sa2_2021 AS tab
-  INNER JOIN raw_admin_bdys_202108.aus_state AS ste ON tab.state_pid = ste.state_pid;
+  FROM raw_admin_bdys.aus_sa2_2021 AS tab
+  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
 
-ALTER TABLE admin_bdys_202108.abs_2021_sa2 ADD CONSTRAINT abs_2021_sa2_pk PRIMARY KEY (gid);
-CREATE INDEX abs_2021_sa2_geom_idx ON admin_bdys_202108.abs_2021_sa2 USING gist(geom);
-ALTER TABLE admin_bdys_202108.abs_2021_sa2 CLUSTER ON abs_2021_sa2_geom_idx;
+ALTER TABLE admin_bdys.abs_2021_sa2 ADD CONSTRAINT abs_2021_sa2_pk PRIMARY KEY (gid);
+CREATE INDEX abs_2021_sa2_geom_idx ON admin_bdys.abs_2021_sa2 USING gist(geom);
+ALTER TABLE admin_bdys.abs_2021_sa2 CLUSTER ON abs_2021_sa2_geom_idx;
 
 
 -- # ---------------------------------------------------------------------------------
 -- ABS census 2021 - statistical area 3's
 --------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS admin_bdys_202108.abs_2021_sa3 CASCADE;
-CREATE TABLE admin_bdys_202108.abs_2021_sa3 AS
+DROP TABLE IF EXISTS admin_bdys.abs_2021_sa3 CASCADE;
+CREATE TABLE admin_bdys.abs_2021_sa3 AS
 SELECT tab.gid,
        sa3_21ppid,
        tab.dt_create,
@@ -127,20 +127,20 @@ SELECT tab.gid,
        area_sqm,
        loci21_uri::text,
        geom
-  FROM raw_admin_bdys_202108.aus_sa3_2021 AS tab
-  INNER JOIN raw_admin_bdys_202108.aus_state AS ste ON tab.state_pid = ste.state_pid;
+  FROM raw_admin_bdys.aus_sa3_2021 AS tab
+  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
 
-ALTER TABLE admin_bdys_202108.abs_2021_sa3 ADD CONSTRAINT abs_2021_sa3_pk PRIMARY KEY (gid);
-CREATE INDEX abs_2021_sa3_geom_idx ON admin_bdys_202108.abs_2021_sa3 USING gist(geom);
-ALTER TABLE admin_bdys_202108.abs_2021_sa3 CLUSTER ON abs_2021_sa3_geom_idx;
+ALTER TABLE admin_bdys.abs_2021_sa3 ADD CONSTRAINT abs_2021_sa3_pk PRIMARY KEY (gid);
+CREATE INDEX abs_2021_sa3_geom_idx ON admin_bdys.abs_2021_sa3 USING gist(geom);
+ALTER TABLE admin_bdys.abs_2021_sa3 CLUSTER ON abs_2021_sa3_geom_idx;
 
 
 -- # ---------------------------------------------------------------------------------
 -- ABS census 2021 - statistical area 4's
 --------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS admin_bdys_202108.abs_2021_sa4 CASCADE;
-CREATE TABLE admin_bdys_202108.abs_2021_sa4 AS
+DROP TABLE IF EXISTS admin_bdys.abs_2021_sa4 CASCADE;
+CREATE TABLE admin_bdys.abs_2021_sa4 AS
 SELECT tab.gid,
        sa4_21ppid,
        tab.dt_create,
@@ -156,20 +156,20 @@ SELECT tab.gid,
        area_sqm,
        loci21_uri::text,
        geom
-  FROM raw_admin_bdys_202108.aus_sa4_2021 AS tab
-  INNER JOIN raw_admin_bdys_202108.aus_state AS ste ON tab.state_pid = ste.state_pid;
+  FROM raw_admin_bdys.aus_sa4_2021 AS tab
+  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
 
-ALTER TABLE admin_bdys_202108.abs_2021_sa4 ADD CONSTRAINT abs_2021_sa4_pk PRIMARY KEY (gid);
-CREATE INDEX abs_2021_sa4_geom_idx ON admin_bdys_202108.abs_2021_sa4 USING gist(geom);
-ALTER TABLE admin_bdys_202108.abs_2021_sa4 CLUSTER ON abs_2021_sa4_geom_idx;
+ALTER TABLE admin_bdys.abs_2021_sa4 ADD CONSTRAINT abs_2021_sa4_pk PRIMARY KEY (gid);
+CREATE INDEX abs_2021_sa4_geom_idx ON admin_bdys.abs_2021_sa4 USING gist(geom);
+ALTER TABLE admin_bdys.abs_2021_sa4 CLUSTER ON abs_2021_sa4_geom_idx;
 
 
 -- # ---------------------------------------------------------------------------------
 -- ABS census 2021 - greater capital city statistical areas
 --------------------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS admin_bdys_202108.abs_2021_gccsa CASCADE;
-CREATE TABLE admin_bdys_202108.abs_2021_gccsa AS
+DROP TABLE IF EXISTS admin_bdys.abs_2021_gccsa CASCADE;
+CREATE TABLE admin_bdys.abs_2021_gccsa AS
 SELECT tab.gid,
        gcc_21ppid,
        tab.dt_create,
@@ -182,9 +182,9 @@ SELECT tab.gid,
        area_sqm,
        loci21_uri::text,
        geom
-  FROM raw_admin_bdys_202108.aus_gccsa_2021 AS tab
-  INNER JOIN raw_admin_bdys_202108.aus_state AS ste ON tab.state_pid = ste.state_pid;
+  FROM raw_admin_bdys.aus_gccsa_2021 AS tab
+  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
 
-ALTER TABLE admin_bdys_202108.abs_2021_gccsa ADD CONSTRAINT abs_2021_gccsa_pk PRIMARY KEY (gid);
-CREATE INDEX abs_2021_gccsa_geom_idx ON admin_bdys_202108.abs_2021_gccsa USING gist(geom);
-ALTER TABLE admin_bdys_202108.abs_2021_gccsa CLUSTER ON abs_2021_gccsa_geom_idx;
+ALTER TABLE admin_bdys.abs_2021_gccsa ADD CONSTRAINT abs_2021_gccsa_pk PRIMARY KEY (gid);
+CREATE INDEX abs_2021_gccsa_geom_idx ON admin_bdys.abs_2021_gccsa USING gist(geom);
+ALTER TABLE admin_bdys.abs_2021_gccsa CLUSTER ON abs_2021_gccsa_geom_idx;
