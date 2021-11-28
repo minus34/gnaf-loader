0cc29587735c6178270cb6c0db11c3b0cd05a69d
Hugh Saalmans <hugh.saalmans@gmail.com>
2021-11-29 08:43:05 +1100

    fixed bug in missing coords workaround

--- postgres-scripts/01-04-raw-gnaf-fix-missing-geocodes.sql
+++ postgres-scripts/01-04-raw-gnaf-fix-missing-geocodes.sql
@@ -1,22 +1,22 @@
 -- workaround for missing default coordinates - 202111 release issue
 with missing as (
     select address_detail_pid
-    from raw_gnaf_202111.address_default_geocode
+    from raw_gnaf.address_default_geocode
     where latitude is null or longitude is null
 ), site as (
-    select gnaf.address_detail_pid,
-           gnaf.address_site_pid
-    from raw_gnaf_202111.address_detail as gnaf
-    inner join missing on gnaf.address_detail_pid = missing.address_detail_pid
+    select adr.address_detail_pid,
+           adr.address_site_pid
+    from raw_gnaf.address_detail as adr
+    inner join missing on adr.address_detail_pid = missing.address_detail_pid
 ), coords as (
     select site.address_detail_pid,
            geo.latitude,
            geo.longitude
-    from raw_gnaf_202111.address_site_geocode as geo
+    from raw_gnaf.address_site_geocode as geo
     inner join site on geo.address_site_pid = site.address_site_pid
     where geocode_type_code = 'PAPS'
 )
-update raw_gnaf_202111.address_default_geocode as def
+update raw_gnaf.address_default_geocode as def
     set latitude = coords.latitude,
         longitude = coords.longitude
 from coords
