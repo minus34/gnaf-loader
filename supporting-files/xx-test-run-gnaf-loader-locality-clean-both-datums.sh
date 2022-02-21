697872ba668af783a316376e2c426cf264686872
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-02-21 13:06:25 +1100

    updates for 202202 release

--- supporting-files/xx-test-run-gnaf-loader-locality-clean-both-datums.sh
+++ supporting-files/xx-test-run-gnaf-loader-locality-clean-both-datums.sh
@@ -21,12 +21,12 @@ echo "--------------------------------------------------------------------------
 echo "Run gnaf-loader and locality boundary clean"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-python3 /Users/$(whoami)/git/minus34/gnaf-loader/load-gnaf.py --pgport=5432 --pgdb=geo --max-processes=6 --gnaf-tables-path="${GNAF_PATH}" --admin-bdys-path="${BDYS_PATH}" --gnaf-schema gnaf_202202_gda94 --admin-schema admin_bdys_202202_gda94 --previous-gnaf-schema gnaf_202202 --previous-admin-schema admin_bdys_202202
-python3 /Users/$(whoami)/git/iag_geo/psma-admin-bdys/locality-clean.py --pgport=5432 --pgdb=geo --max-processes=6 --output-path=${OUTPUT_FOLDER} --admin-schema admin_bdys_202202_gda94
+python3 /Users/$(whoami)/git/minus34/gnaf-loader/load-gnaf.py --pgport=5432 --pgdb=geo --max-processes=6 --gnaf-tables-path="${GNAF_PATH}" --admin-bdys-path="${BDYS_PATH}" --gnaf-schema gnaf_202202 --admin-schema admin_bdys_202202 --previous-gnaf-schema gnaf_202111 --previous-admin-schema admin_bdys_202111
+python3 /Users/$(whoami)/git/iag_geo/psma-admin-bdys/locality-clean.py --pgport=5432 --pgdb=geo --max-processes=6 --output-path=${OUTPUT_FOLDER} --admin-schema admin_bdys_202202
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "Run gnaf-loader and locality boundary clean - GDA2020"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-python3 /Users/$(whoami)/git/minus34/gnaf-loader/load-gnaf.py --pgport=5432 --pgdb=geo --max-processes=6 --gnaf-tables-path="${GNAF_2020_PATH}" --admin-bdys-path="${BDYS_2020_PATH}" --srid=7844 --gnaf-schema gnaf_202202_gda2020 --admin-schema admin_bdys_202202_gda2020 --previous-gnaf-schema gnaf_202202_gda94 --previous-admin-schema admin_bdys_202202_gda94
+python3 /Users/$(whoami)/git/minus34/gnaf-loader/load-gnaf.py --pgport=5432 --pgdb=geo --max-processes=6 --gnaf-tables-path="${GNAF_2020_PATH}" --admin-bdys-path="${BDYS_2020_PATH}" --srid=7844 --gnaf-schema gnaf_202202_gda2020 --admin-schema admin_bdys_202202_gda2020 --previous-gnaf-schema gnaf_202202 --previous-admin-schema admin_bdys_202202
 python3 /Users/$(whoami)/git/iag_geo/psma-admin-bdys/locality-clean.py --pgport=5432 --pgdb=geo --max-processes=6 --output-path=${OUTPUT_FOLDER} --admin-schema admin_bdys_202202_gda2020
