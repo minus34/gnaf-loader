552b61129026176093db6eda74e7f8bea771f053
Hugh Saalmans <hugh.saalmans@gmail.com>
2022-02-23 22:13:10 +1100

    refactored to fix missing digest issue

--- supporting-files/quarterly_processing/04-create-docker-images.sh
+++ supporting-files/quarterly_processing/04-create-docker-images.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 # get the directory this script is running from
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -9,19 +8,15 @@ echo "build gnaf-loader docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
 cd ${SCRIPT_DIR}/../../docker
-docker build --squash --tag minus34/gnafloader:latest --tag minus34/gnafloader:202202 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202202" .
+docker build --tag minus34/gnafloader:latest --tag minus34/gnafloader:202202 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202202" .
+docker push --tag minus34/gnafloader:latest --tag minus34/gnafloader:202202
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "build gnaf-loader GDA2020 docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-docker build --squash --tag minus34/gnafloader:latest-gda2020 --tag minus34/gnafloader:202202-gda2020 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202202-gda2020" .
-
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo "push images to Docker Hub"
-echo "---------------------------------------------------------------------------------------------------------------------"
-
-docker push --all-tags minus34/gnafloader
+docker build --tag minus34/gnafloader:latest-gda2020 --tag minus34/gnafloader:202202-gda2020 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202202-gda2020" .
+docker push --tag minus34/gnafloader:latest-gda2020--tag minus34/gnafloader:202202-gda2020
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "clean up Docker locally - warning: this could accidentally destroy other Docker images"
