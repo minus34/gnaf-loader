b211ed41533420e317466ca37c7bfe4e310e0838
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-06-01 10:22:47 +1000

    reverted build method

--- testing/geoparquet/xx_gdal_build.sh
+++ testing/geoparquet/xx_gdal_build.sh
@@ -5,15 +5,15 @@
 conda deactivate
 #conda activate gdal
 
-BUILD_DIR="/Users/$(whoami)/gdal-3.5.0/build"
+INSTALL_DIR="/Users/$(whoami)/gdal-3.5.0"
 
 echo "-------------------------------------------------------------------------"
 echo "Installing CMake and prerequisites"
 echo "-------------------------------------------------------------------------"
 
-# get Homebrew packages
-brew update
-brew install openssl cmake apache-arrow protobuf sqlite
+## get Homebrew packages
+#brew update
+#brew install openssl cmake apache-arrow protobuf sqlite
 
 echo "-------------------------------------------------------------------------"
 echo "Downloading GDAL source code"
@@ -23,27 +23,28 @@ echo "-------------------------------------------------------------------------"
 cd ${HOME}
 
 # WARNING: delete existing source code & build directory
-rm -rf ../${BUILD_DIR}
+rm -rf ${INSTALL_DIR}
 
-curl -O -L https://github.com/OSGeo/gdal/releases/download/v3.5.0/gdal-3.5.0.tar.gz
+## NOTE: using insecure to allow for man-in-the-middle corporate proxies
+#curl -O -L --insecure https://github.com/OSGeo/gdal/releases/download/v3.5.0/gdal-3.5.0.tar.gz
 tar -xzf gdal-3.5.0.tar.gz
-rm gdal-3.5.0.tar.gz
+#rm gdal-3.5.0.tar.gz
 
 echo "-------------------------------------------------------------------------"
 echo "Building GDAL with Parquet supported"
 echo "-------------------------------------------------------------------------"
 
-mkdir -p ${BUILD_DIR}
-cd ${BUILD_DIR}
+mkdir -p ${INSTALL_DIR}/build
+cd ${INSTALL_DIR}/build
 #cd ..
 
-cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/apache-arrow/8.0.0_1/lib;/usr/local/Cellar/protobuf/3.19.4;/usr/local/Cellar/openssl@1.1/1.1.1o;/usr/local/Cellar/sqlite/3.38.5" \
-      -DGDAL_USE_OPENCL=ON \
-      -DGDAL_USE_GEOTIFF_INTERNAL=ON \
-      -DCMAKE_BUILD_TYPE=Release \
-      .. | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_1.log
-cmake --build . | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_2.log
-cmake --build . --target install | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_3.log
+#cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/apache-arrow/8.0.0_1/lib;/usr/local/Cellar/protobuf/3.19.4;/usr/local/Cellar/openssl@1.1/1.1.1o;/usr/local/Cellar/sqlite/3.38.5" \
+#      -DGDAL_USE_OPENCL=ON \
+#      -DGDAL_USE_GEOTIFF_INTERNAL=ON \
+#      -DCMAKE_BUILD_TYPE=Release \
+#      .. | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_1.log
+#cmake --build . | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_2.log
+#cmake --build . --target install | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_3.log
 
 #/usr/local/Cellar/apache-arrow/8.0.0_1/lib/cmake/arrow
 
@@ -53,11 +54,12 @@ cmake --build . --target install | tee /Users/s57405/git/minus34/gnaf-loader/tes
 #cmake --build build --config Release -j 8 | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_ver2_2.log
 
 
-#cmake -DCMAKE_PREFIX_PATH:FILEPATH="${CONDA_PREFIX}" \
-#      .. | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_1.log
-#cmake --build . | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_2.log
-#cmake --build . --target install | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_3.log
+cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/apache-arrow/8.0.0_1/lib;/usr/local/Cellar/protobuf/3.19.4;/usr/local/Cellar/openssl@3/3.0.3;/usr/local/Cellar/sqlite/3.38.5" \
+      .. | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_1.log
+cmake --build . | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_2.log
+cmake --build . --target install | tee /Users/s57405/git/minus34/gnaf-loader/testing/geoparquet/xx_gdal_build_3.log
 
 
+#-DCMAKE_PREFIX_PATH:FILEPATH="/usr/local/Cellar" \
 #                    -DCMAKE_C_COMPILER_LAUNCHER=clcache \
 #                    -DCMAKE_CXX_COMPILER_LAUNCHER=clcache \
