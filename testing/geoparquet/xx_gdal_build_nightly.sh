a733d3c7b5a82ebac32dfc65e97c4c6a6d60c0d9
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-06-10 16:10:11 +1000

    trying conda env

--- testing/geoparquet/xx_gdal_build_nightly.sh
+++ testing/geoparquet/xx_gdal_build_nightly.sh
@@ -4,6 +4,7 @@
 # requires Homebrew: https://brew.sh/
 
 # TODO: edit these
+PYTHON_VERSION="3.10"
 INSTALL_DIR="/Users/$(whoami)/git/osgeo"
 LOG_DIR="/Users/$(whoami)/git/minus34/gnaf-loader/testing/geoparquet"
 
@@ -11,9 +12,36 @@ echo "-------------------------------------------------------------------------"
 echo "Installing CMake and prerequisites"
 echo "-------------------------------------------------------------------------"
 
-# get Homebrew packages
-brew update
-brew install openssl cmake apache-arrow protobuf sqlite proj tiledb-inc/stable/tiledb
+## get Homebrew packages
+#brew update
+#brew install openssl cmake apache-arrow protobuf sqlite proj tiledb-inc/stable/tiledb
+
+# WARNING - removes existing environment
+conda deactivate
+conda env remove --name gdal
+
+# update Conda platform & install Mamba (much faster package installer)
+conda update -y conda
+conda install -y -c conda-forge mamba -n base
+
+# Create Conda environment
+conda create -y -n gdal python=${PYTHON_VERSION}
+
+# activate and setup env
+conda activate gdal
+conda config --env --add channels conda-forge
+conda config --env --set channel_priority strict
+
+# reactivate for env vars to take effect
+conda activate gdal
+
+mamba install --yes --quiet curl libiconv icu git swig numpy pytest zlib clcache
+mamba install --yes --quiet -c conda-forge compilers
+mamba install --yes --quiet -c conda-forge \
+    cmake proj geos protobuf arrow-cpp hdf4 hdf5 \
+    libnetcdf openjpeg poppler libtiff libpng xerces-c expat libxml2 kealib json-c \
+    cfitsio freexl geotiff jpeg libpq libspatialite libwebp-base pcre postgresql \
+    sqlite tiledb zstd charls cryptopp cgal librttopo libkml openssl xz
 
 echo "-------------------------------------------------------------------------"
 echo "Downloading GDAL source code"
@@ -38,8 +66,14 @@ echo "-------------------------------------------------------------------------"
 cd "${INSTALL_DIR}/gdal/build"
 
 # TODO: need to edit package version numbers in future
-cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/apache-arrow/8.0.0_1/lib;/usr/local/Cellar/protobuf/3.19.4;/usr/local/Cellar/openssl@3/3.0.3;/usr/local/Cellar/sqlite/3.38.5;/usr/local/Cellar/tiledb/2.5.0" \
-      -DPROJ_INCLUDE_DIR="/usr/local/Cellar/proj/9.0.0_1/include" \
-      .. | tee ${LOG_DIR}/xx_gdal_nightly_build_1.log
+#cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/apache-arrow/8.0.0_1/lib;/usr/local/Cellar/protobuf/3.19.4;/usr/local/Cellar/openssl@3/3.0.3;/usr/local/Cellar/sqlite/3.38.5;/usr/local/Cellar/tiledb/2.5.0" \
+#      -DPROJ_INCLUDE_DIR="/usr/local/Cellar/proj/9.0.0_1/include" \
+#      -DPython_ROOT=""
+#      .. | tee ${LOG_DIR}/xx_gdal_nightly_build_1.log
+#cmake --build . | tee ${LOG_DIR}/xx_gdal_nightly_build_2.log
+#cmake --build . --target install | tee ${LOG_DIR}/xx_gdal_nightly_build_3.log
+
+
+cmake .. | tee ${LOG_DIR}/xx_gdal_nightly_build_1.log
 cmake --build . | tee ${LOG_DIR}/xx_gdal_nightly_build_2.log
 cmake --build . --target install | tee ${LOG_DIR}/xx_gdal_nightly_build_3.log
