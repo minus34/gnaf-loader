fc8eb7ced924bec5af93201c0f0e457f8a6a409e
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-11-17 17:36:32 +1100

    added export to CSV with CSVT file

---
+++ testing/xx_export_to_csv.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# need a Python 3.10+ environment with GDAL 3.6.0 and PyArrow
+conda deactivate
+conda activate geo
+
+# get the directory this script is running from
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+OUTPUT_FOLDER="/Users/$(whoami)/tmp/gdal-testing"
+mkdir -p "${OUTPUT_FOLDER}"
+cd "${OUTPUT_FOLDER}"
+
+
+# convert Postgres table to CSV with CSVT field types file
+input_schema="gnaf_202208"
+input_table="address_principals"
+
+echo "Exporting ${input_schema}.${input_table}"
+
+ogr2ogr \
+  -f CSV \
+  "${OUTPUT_FOLDER}/${input_table}.csv" \
+  PG:"host='localhost' dbname='geo' user='postgres' password='password' port='5432'" \
+  "${input_schema}.${input_table}(geom)" \
+  -lco CREATE_CSVT=YES
