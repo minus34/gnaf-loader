7e6179fbb45da32996f5472265482ed941a933de
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-11-23 13:47:31 +1100

    added download

--- supporting-files/restore-gnaf-admin-bdys.sh
+++ supporting-files/restore-gnaf-admin-bdys.sh
@@ -8,8 +8,15 @@ echo "--------------------------------------------------------------------------
 
 psql -d geo -p 5432 -U postgres -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 
-/Applications/Postgres.app/Contents/Versions/13/bin/pg_restore -Fc -d geo -p 5432 -U postgres /Users/$(whoami)/Downloads/gnaf-202211.dmp
-/Applications/Postgres.app/Contents/Versions/13/bin/pg_restore -Fc -d geo -p 5432 -U postgres /Users/$(whoami)/Downloads/admin-bdys-202211.dmp
+cd /Users/$(whoami)/Downloads
+
+curl --insecure https://minus34.com/opendata/geoscape-202211/gnaf-202211.dmp --output ./gnaf-202211.dmp
+/Applications/Postgres.app/Contents/Versions/13/bin/pg_restore -Fc -d geo -p 5432 -U postgres ./gnaf-202211.dmp
+rm ./gnaf-202211.dmp
+
+curl --insecure https://minus34.com/opendata/geoscape-202211/admin-bdys-202211.dmp --output ./admin-bdys-202211.dmp
+/Applications/Postgres.app/Contents/Versions/13/bin/pg_restore -Fc -d geo -p 5432 -U postgres ./admin-bdys-202211.dmp
+rm ./admin-bdys-202211.dmp
 
 duration=$SECONDS
 
