1a5082cc10212bd43b826d9703fdb3856332df6c
Hugh Saalmans <hugh.saalmans@gmail.com>
2023-02-22 15:53:25 +1100

    converted to podman

--- supporting-files/quarterly_processing/04-create-docker-images.sh
+++ supporting-files/quarterly_processing/04-create-docker-images.sh
@@ -9,19 +9,33 @@ echo "--------------------------------------------------------------------------
 echo "build gnaf-loader GDA94 docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-docker build --tag minus34/gnafloader:latest --tag minus34/gnafloader:202302 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202302" .
+podman build --tag docker.io/minus34/gnafloader:latest --tag minus34/gnafloader:202302 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202302" .
+
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "push image (with 2 tags) to Docker Hub"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+# login (if needed)
+#podman login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} docker.io
+
+podman push docker.io/minus34/gnafloader/latest
+podman push docker.io/minus34/gnafloader/202302
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "build gnaf-loader GDA2020 docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-docker build --tag minus34/gnafloader:latest-gda2020 --tag minus34/gnafloader:202302-gda2020 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202302-gda2020" .
+podman build --tag docker.io/minus34/gnafloader:latest-gda2020 --tag minus34/gnafloader:202302-gda2020 --no-cache --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202302-gda2020" .
 
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "push both images (with 4 tags) to Docker Hub"
+echo "push image (with 2 tags) to Docker Hub"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-docker push minus34/gnafloader --all-tags
+# login (if needed)
+#podman login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} docker.io
+
+podman push docker.io/minus34/gnafloader/latest-gda2020
+podman push docker.io/minus34/gnafloader/202302-gda2020
 
 #echo "---------------------------------------------------------------------------------------------------------------------"
 #echo "clean up Docker locally - warning: this could accidentally destroy other Docker images"
