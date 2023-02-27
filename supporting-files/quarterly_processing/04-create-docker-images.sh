e3e1c6abaabd29ee64afe786c16deca78b21d91e
Hugh Saalmans <hugh.saalmans@gmail.com>
2023-02-27 18:14:28 +1100

    fixed out of space error

--- supporting-files/quarterly_processing/04-create-docker-images.sh
+++ supporting-files/quarterly_processing/04-create-docker-images.sh
@@ -10,7 +10,7 @@ echo "setup Podman machine"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
 podman machine stop
-podman machine set --cpus=8 --memory=12288 --disk-size 100
+podman machine set --cpus=8 --memory=12288 --disk-size 200
 podman machine start
 
 #echo 'y' | podman machine rm gnaf-vm
@@ -20,7 +20,7 @@ echo "--------------------------------------------------------------------------
 echo "build gnaf-loader GDA94 docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
-podman build --squash-all --tag docker.io/minus34/gnafloader:latest --tag docker.io/minus34/gnafloader:202302 --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202302" .
+podman build --no-cache --squash-all --tag docker.io/minus34/gnafloader:latest --tag docker.io/minus34/gnafloader:202302 --build-arg BASE_URL="https://minus34.com/opendata/geoscape-202302" .
 
 #echo "---------------------------------------------------------------------------------------------------------------------"
 #echo "push image (with 2 tags) to Docker Hub"
