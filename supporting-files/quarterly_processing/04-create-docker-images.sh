1de825fbfa8b19ec51223471450a3495c9e9bf1c
Hugh Saalmans <hugh.saalmans@gmail.com>
2024-12-25 19:37:13 +1100

    docker arm64 testing

--- supporting-files/quarterly_processing/04-create-docker-images.sh
+++ supporting-files/quarterly_processing/04-create-docker-images.sh
@@ -3,40 +3,49 @@
 # get the directory this script is running from
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-DATA_FOLDER="/Users/$(whoami)/tmp/geoscape_202411"
-DATA_FOLDER_2020="/Users/$(whoami)/tmp/geoscape_202411_gda2020"
+OUTPUT_FOLDER="/Users/$(whoami)/tmp/geoscape_202411"
+OUTPUT_FOLDER_2020="/Users/$(whoami)/tmp/geoscape_202411_gda2020"
 
-cd ${SCRIPT_DIR}/../../docker
+DOCKER_FOLDER=${SCRIPT_DIR}/../../docker
+
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "copy postgres dump files to Dockerfile folder"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+cp ${OUTPUT_FOLDER}/*.dmp ${DOCKER_FOLDER}/
 
 #echo "---------------------------------------------------------------------------------------------------------------------"
 #echo "start Docker desktop and wait 90 seconds for startup"
 #echo "---------------------------------------------------------------------------------------------------------------------"
-#
+
+cd ${DOCKER_FOLDER}
+
 #open -a Docker
 #sleep 90
 
-# required or Docker VM will run out of space
-echo 'y' | docker builder prune --all
-echo 'y' | docker system prune --all
+## required or Docker VM will run out of space
+#echo 'y' | docker builder prune --all
+#echo 'y' | docker system prune --all
+#echo 'y' | docker buildx rm --all-inactive
 
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "build gnaf-loader GDA94 docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
 # 1. go to Dockerfile directory
-#cd ${DATA_FOLDER}
+cd ${OUTPUT_FOLDER}
 
 # 2. launch buildx
 docker buildx create --name gnafloader_test_builder --use
 docker buildx inspect --bootstrap
 
 # 3. build and push images
-docker buildx build --platform linux/amd64,linux/arm64 --tag minus34/gnafloader_test:latest --tag minus34/gnafloader_test:202411 --build-arg DATA_DIR=${DATA_FOLDER} . --load # --push
+docker buildx build --platform linux/amd64,linux/arm64 --tag minus34/gnafloader_test:latest --tag minus34/gnafloader_test:202411 -f ${DOCKER_FOLDER}/Dockerfile . --load # --push
 
-echo "---------------------------------------------------------------------------------------------------------------------"
-echo "clean up Docker locally - warning: this could accidentally destroy other Docker images"
-echo "---------------------------------------------------------------------------------------------------------------------"
-
+#echo "---------------------------------------------------------------------------------------------------------------------"
+#echo "clean up Docker locally - warning: this could accidentally destroy other Docker images"
+#echo "---------------------------------------------------------------------------------------------------------------------"
+#
 ## required or Docker VM will run out of space
 #echo 'y' | docker builder prune --all
 #echo 'y' | docker system prune --all
@@ -62,3 +71,4 @@ echo "--------------------------------------------------------------------------
 ## required or Docker VM will run out of space
 #echo 'y' | docker builder prune --all
 #echo 'y' | docker system prune --all
+#echo 'y' | docker buildx rm --all-inactive
