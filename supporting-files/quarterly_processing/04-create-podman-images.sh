2289f7627e27de52bdeda1c710d743366baa371c
Hugh Saalmans <hugh.saalmans@gmail.com>
2024-12-28 16:21:46 +1100

    podman multi-platform testing

--- supporting-files/quarterly_processing/04-create-podman-images.sh
+++ supporting-files/quarterly_processing/04-create-podman-images.sh
@@ -8,6 +8,9 @@ OUTPUT_FOLDER_2020="/Users/$(whoami)/tmp/geoscape_202411_gda2020"
 
 DOCKER_FOLDER=${SCRIPT_DIR}/../../docker
 
+# default podman temp folder /var/tmp/ is too small
+export TMPDIR=/Users/$(whoami)/tmp/podman/
+
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo "copy postgres dump files to Dockerfile folder"
 echo "---------------------------------------------------------------------------------------------------------------------"
@@ -15,11 +18,8 @@ echo "--------------------------------------------------------------------------
 cp ${OUTPUT_FOLDER}/*.dmp ${DOCKER_FOLDER}/
 
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo "start podman"
+echo "start podman - warning: this could accidentally destroy other images"
 echo "---------------------------------------------------------------------------------------------------------------------"
-
-# default folder /var/tmp/ is too small
-export TMPDIR=/Users/$(whoami)/tmp/podman/
 
 podman machine stop
 echo 'y' | podman system prune --all
@@ -36,58 +36,55 @@ echo "build gnaf-loader GDA94 docker image"
 echo "---------------------------------------------------------------------------------------------------------------------"
 
 # build images
-podman build --platform linux/amd64,linux/arm64 --tag localhost/fred .
-
-podman manifest create minus34/gnafloader_test:latest localhost/fred
-podman manifest push minus34/gnafloader_test:latest
-
-docker manifest create minus34/gnafloader_test:202411 localhost/fred
-docker manifest push minus34/gnafloader_test:202411
-
-#podman build --platform linux/amd64,linux/arm64 --tag docker.io/minus34/gnafloader_test:latest --tag docker.io/minus34/gnafloader_test:202411 .
-#
-#podman manifest create minus34/gnafloader_test
-#podman manifest add minus34/gnafloader_test docker.io/minus34/gnafloader_test:latest
-#podman manifest add minus34/gnafloader_test docker.io/minus34/gnafloader_test:202411
-#podman manifest push minus34/gnafloader_test
-
-
-
-
-#podman build --no-cache --platform linux/arm64,linux/amd64 --tag minus34/gnafloader_test:latest --tag minus34/gnafloader_test:202411 .
-#podman image push localhost/minus34/gnafloader_test docker://docker.io/minus34/gnafloader_test
+podman manifest create localhost/gnafloader_test
+podman build --platform linux/amd64,linux/arm64 --manifest localhost/gnafloader_test .
+podman manifest push localhost/gnafloader_test docker://docker.io/minus34/gnafloader_test:latest
+podman manifest push localhost/gnafloader_test docker://docker.io/minus34/gnafloader_test:202411
 
 # delete postgres dmp files
 rm ${DOCKER_FOLDER}/*.dmp
 
-#podman machine stop
-#echo 'y' | podman system prune --all
-#echo 'y' | podman machine rm
-#podman machine init --cpus 10 --memory 16384 --disk-size=128  # memory in Mb, disk size in Gb
-#podman machine start
-#podman login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} docker.io/minus34/gnafloader_test
-#
-#echo "---------------------------------------------------------------------------------------------------------------------"
-#echo "copy GDA2020 postgres dump files to Dockerfile folder"
-#echo "---------------------------------------------------------------------------------------------------------------------"
-#
-#cp ${OUTPUT_FOLDER_2020}/*.dmp ${DOCKER_FOLDER}/
-#
-#echo "---------------------------------------------------------------------------------------------------------------------"
-#echo "build gnaf-loader GDA2020 docker image"
-#echo "---------------------------------------------------------------------------------------------------------------------"
-#
-#podman build --no-cache --platform linux/arm64,linux/amd64 --tag minus34/gnafloader_test:latest-gda2020 --tag minus34/gnafloader_test:202411-gda2020 .
-#podman image push localhost/minus34/gnafloader_test docker://docker.io/minus34/gnafloader_test
-#
-## delete postgres dmp files
-#rm ${DOCKER_FOLDER}/*.dmp
-#
-#echo "---------------------------------------------------------------------------------------------------------------------"
-#echo "clean up podman locally - warning: this could accidentally destroy other images"
-#echo "---------------------------------------------------------------------------------------------------------------------"
-#
-## required or podman VM could run out of space
-#echo 'y' | podman system prune --all
-#podman machine stop
-#echo 'y' | podman machine rm
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "copy GDA2020 postgres dump files to Dockerfile folder"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+cp ${OUTPUT_FOLDER_2020}/*.dmp ${DOCKER_FOLDER}/
+
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "start podman - warning: this could accidentally destroy other images"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+podman machine stop
+echo 'y' | podman system prune --all
+echo 'y' | podman machine rm
+podman machine init --cpus 10 --memory 16384 --disk-size=128  # memory in Mb, disk size in Gb
+podman machine start
+podman login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} docker.io/minus34
+
+# go to Dockerfile directory
+cd ${DOCKER_FOLDER}
+
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "build gnaf-loader GDA2020 docker image"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+# build images
+podman manifest create localhost/gnafloader_test
+podman build --platform linux/amd64,linux/arm64 --manifest localhost/gnafloader_test .
+podman manifest push localhost/gnafloader_test docker://docker.io/minus34/gnafloader_test:latest-gda2020
+podman manifest push localhost/gnafloader_test docker://docker.io/minus34/gnafloader_test:202411-gda2020
+
+# delete postgres dmp files
+rm ${DOCKER_FOLDER}/*.dmp
+
+# delete postgres dmp files
+rm ${DOCKER_FOLDER}/*.dmp
+
+echo "---------------------------------------------------------------------------------------------------------------------"
+echo "clean up podman locally - warning: this could accidentally destroy other images"
+echo "---------------------------------------------------------------------------------------------------------------------"
+
+# clean up
+echo 'y' | podman system prune --all
+podman machine stop
+echo 'y' | podman machine rm
