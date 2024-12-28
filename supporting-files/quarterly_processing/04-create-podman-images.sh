747e89bb04028c70031662de82eeecb60cfd7372
Hugh Saalmans <hugh.saalmans@gmail.com>
2024-12-29 09:09:17 +1100

    podman multi-platform testing complete

--- supporting-files/quarterly_processing/04-create-podman-images.sh
+++ supporting-files/quarterly_processing/04-create-podman-images.sh
@@ -26,7 +26,7 @@ echo "--------------------------------------------------------------------------
 echo 'y' | podman system prune --all
 podman machine stop
 echo 'y' | podman machine rm
-podman machine init --cpus 10 --memory 16384 --disk-size=128  # memory in Mb, disk size in Gb
+podman machine init --cpus 10 --memory 16384 --disk-size=256  # memory in Mb, disk size in Gb
 podman machine start
 podman login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} docker.io/minus34
 
@@ -69,7 +69,7 @@ echo "--------------------------------------------------------------------------
 echo 'y' | podman system prune --all
 podman machine stop
 echo 'y' | podman machine rm
-podman machine init --cpus 10 --memory 16384 --disk-size=128  # memory in Mb, disk size in Gb
+podman machine init --cpus 10 --memory 16384 --disk-size=256  # memory in Mb, disk size in Gb
 podman machine start
 podman login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD} docker.io/minus34
 
