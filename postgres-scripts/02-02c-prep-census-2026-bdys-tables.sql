2ea3ded9892fdd30fde724b34ebb0b5561cbf054
Hugh Saalmans <hugh.saalmans@gmail.com>
2026-08-24 21:09:59 +1000

    Census 2026 bdys working

--- postgres-scripts/02-02c-prep-census-2026-bdys-tables.sql
+++ postgres-scripts/02-02c-prep-census-2026-bdys-tables.sql
@@ -5,32 +5,30 @@
 
 DROP TABLE IF EXISTS admin_bdys.abs_2026_mb CASCADE;
 CREATE TABLE admin_bdys.abs_2026_mb AS
-SELECT tab.gid,
-       mb_26ppid,
-       tab.dt_create,
-       mb_26pid::text,
-       mb26_code::bigint,
-       mb_cat::text,
-       chng_flag::integer,
-       chng_label::text,
-       sa1_26pid::text,
-       sa1_26code,
-       sa2_26code,
-       sa2_26name::text,
-       sa3_26code,
-       sa3_26name::text,
-       sa4_26code,
-       sa4_26name::text,
-       gcc_26code::text,
-       gcc_26name::text,
-       ste.st_abbrev::text AS state,
-       area_sqm,
-       mb26_dwell,
-       mb26_pop,
-       loci26_uri::text,
+SELECT gid,
+       mb_ply_26::text,
+       dt_create,
+       mb_pid_26::text,
+       mb_code_26::bigint,
+       mb_cat_26::text,
+       chn_flg_26::integer,
+       chn_lbl_26::text,
+       s1_pid_26::text,
+       s1_code_26,
+       s2_code_26,
+       s2_name_26::text,
+       s3_code_26,
+       s3_name_26::text,
+       s4_code_26,
+       s4_name_26::text,
+       gc_code_26::text,
+       gc_name_26::text,
+       state,
+       mb_ar_sqkm,
+      --  mb26_dwell,
+      --  mb26_pop,
        geom
-  FROM raw_admin_bdys.aus_mb_2026 AS tab
-  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
+FROM raw_admin_bdys.aus_mb_2026;
 
 ALTER TABLE admin_bdys.abs_2026_mb ADD CONSTRAINT abs_2026_mb_pk PRIMARY KEY (gid);
 CREATE INDEX abs_2026_mb_geom_idx ON admin_bdys.abs_2026_mb USING gist(geom);
@@ -43,28 +41,26 @@ ALTER TABLE admin_bdys.abs_2026_mb CLUSTER ON abs_2026_mb_geom_idx;
 
 DROP TABLE IF EXISTS admin_bdys.abs_2026_sa1 CASCADE;
 CREATE TABLE admin_bdys.abs_2026_sa1 AS
-SELECT tab.gid,
-       sa1_26ppid,
-       tab.dt_create,
-       sa1_26pid,
-       sa1_26code,
-       chng_flag,
-       chng_label::text,
-       sa2_26pid,
-       sa2_26code,
-       sa2_26name::text,
-       sa3_26code,
-       sa3_26name::text,
-       sa4_26code,
-       sa4_26name::text,
-       gcc_26code,
-       gcc_26name::text,
-       ste.st_abbrev::text AS state,
-       area_sqm,
-       loci26_uri::text,
+SELECT gid,
+       s1_ply_26,
+       dt_create,
+       s1_pid_26,
+       s1_code_26,
+       chn_flg_26,
+       chn_lbl_26::text,
+       s2_pid_26,
+       s2_code_26,
+       s2_name_26::text,
+       s3_code_26,
+       s3_name_26::text,
+       s4_code_26,
+       s4_name_26::text,
+       gc_code_26,
+       gc_name_26::text,
+       state,
+       s1_ar_sqkm,
        geom
-  FROM raw_admin_bdys.aus_sa1_2026 AS tab
-  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
+FROM raw_admin_bdys.aus_sa1_2026;
 
 ALTER TABLE admin_bdys.abs_2026_sa1 ADD CONSTRAINT abs_2026_sa1_pk PRIMARY KEY (gid);
 CREATE INDEX abs_2026_sa1_geom_idx ON admin_bdys.abs_2026_sa1 USING gist(geom);
@@ -77,27 +73,25 @@ ALTER TABLE admin_bdys.abs_2026_sa1 CLUSTER ON abs_2026_sa1_geom_idx;
 
 DROP TABLE IF EXISTS admin_bdys.abs_2026_sa2 CASCADE;
 CREATE TABLE admin_bdys.abs_2026_sa2 AS
-SELECT tab.gid,
-       sa2_26ppid,
-       tab.dt_create,
-       sa2_26pid,
-       sa2_26code,
-       sa2_26name::text,
-       chng_flag,
-       chng_label::text,
-       sa3_26pid,
-       sa3_26code,
-       sa3_26name::text,
-       sa4_26code,
-       sa4_26name::text,
-       gcc_26code,
-       gcc_26name::text,
-       ste.st_abbrev::text AS state,
-       area_sqm,
-       loci26_uri::text,
+SELECT gid,
+       s2_ply_26,
+       dt_create,
+       s2_pid_26,
+       s2_code_26,
+       s2_name_26::text,
+       chn_flg_26,
+       chn_lbl_26::text,
+       s3_pid_26,
+       s3_code_26,
+       s3_name_26::text,
+       s4_code_26,
+       s4_name_26::text,
+       gc_code_26,
+       gc_name_26::text,
+       state,
+       s2_ar_sqkm,
        geom
-  FROM raw_admin_bdys.aus_sa2_2026 AS tab
-  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
+FROM raw_admin_bdys.aus_sa2_2026;
 
 ALTER TABLE admin_bdys.abs_2026_sa2 ADD CONSTRAINT abs_2026_sa2_pk PRIMARY KEY (gid);
 CREATE INDEX abs_2026_sa2_geom_idx ON admin_bdys.abs_2026_sa2 USING gist(geom);
@@ -110,25 +104,23 @@ ALTER TABLE admin_bdys.abs_2026_sa2 CLUSTER ON abs_2026_sa2_geom_idx;
 
 DROP TABLE IF EXISTS admin_bdys.abs_2026_sa3 CASCADE;
 CREATE TABLE admin_bdys.abs_2026_sa3 AS
-SELECT tab.gid,
-       sa3_26ppid,
-       tab.dt_create,
-       sa3_26pid,
-       sa3_26code,
-       sa3_26name::text,
-       chng_flag,
-       chng_label::text,
-       sa4_26pid,
-       sa4_26code,
-       sa4_26name::text,
-       gcc_26code,
-       gcc_26name::text,
-       ste.st_abbrev::text AS state,
-       area_sqm,
-       loci26_uri::text,
+SELECT gid,
+       s3_ply_26,
+       dt_create,
+       s3_pid_26,
+       s3_code_26,
+       s3_name_26::text,
+       chn_flg_26,
+       chn_lbl_26::text,
+       s4_pid_26,
+       s4_code_26,
+       s4_name_26::text,
+       gc_code_26,
+       gc_name_26::text,
+       state,
+       s3_ar_sqkm,
        geom
-  FROM raw_admin_bdys.aus_sa3_2026 AS tab
-  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
+FROM raw_admin_bdys.aus_sa3_2026;
 
 ALTER TABLE admin_bdys.abs_2026_sa3 ADD CONSTRAINT abs_2026_sa3_pk PRIMARY KEY (gid);
 CREATE INDEX abs_2026_sa3_geom_idx ON admin_bdys.abs_2026_sa3 USING gist(geom);
@@ -141,23 +133,21 @@ ALTER TABLE admin_bdys.abs_2026_sa3 CLUSTER ON abs_2026_sa3_geom_idx;
 
 DROP TABLE IF EXISTS admin_bdys.abs_2026_sa4 CASCADE;
 CREATE TABLE admin_bdys.abs_2026_sa4 AS
-SELECT tab.gid,
-       sa4_26ppid,
-       tab.dt_create,
-       sa4_26pid,
-       sa4_26code,
-       sa4_26name::text,
-       chng_flag,
-       chng_label::text,
-       gcc_26pid,
-       gcc_26code,
-       gcc_26name::text,
-       ste.st_abbrev::text AS state,
-       area_sqm,
-       loci26_uri::text,
+SELECT gid,
+       s4_ply_26,
+       dt_create,
+       s4_pid_26,
+       s4_code_26,
+       s4_name_26::text,
+       chn_flg_26,
+       chn_lbl_26::text,
+       gc_pid_26,
+       gc_code_26,
+       gc_name_26::text,
+       state,
+       s4_ar_sqkm,
        geom
-  FROM raw_admin_bdys.aus_sa4_2026 AS tab
-  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
+FROM raw_admin_bdys.aus_sa4_2026;
 
 ALTER TABLE admin_bdys.abs_2026_sa4 ADD CONSTRAINT abs_2026_sa4_pk PRIMARY KEY (gid);
 CREATE INDEX abs_2026_sa4_geom_idx ON admin_bdys.abs_2026_sa4 USING gist(geom);
@@ -170,20 +160,18 @@ ALTER TABLE admin_bdys.abs_2026_sa4 CLUSTER ON abs_2026_sa4_geom_idx;
 
 DROP TABLE IF EXISTS admin_bdys.abs_2026_gccsa CASCADE;
 CREATE TABLE admin_bdys.abs_2026_gccsa AS
-SELECT tab.gid,
-       gcc_26ppid,
-       tab.dt_create,
-       gcc_26pid,
-       gcc_26code,
-       gcc_26name::text,
-       chng_flag,
-       chng_label::text,
-       ste.st_abbrev::text AS state,
-       area_sqm,
-       loci26_uri::text,
+SELECT gid,
+       gc_ply_26,
+       dt_create,
+       gc_pid_26,
+       gc_code_26,
+       gc_name_26::text,
+       chn_flg_26,
+       chn_lbl_26::text,
+       state,
+       gc_ar_sqkm,
        geom
-  FROM raw_admin_bdys.aus_gccsa_2026 AS tab
-  INNER JOIN raw_admin_bdys.aus_state AS ste ON tab.state_pid = ste.state_pid;
+FROM raw_admin_bdys.aus_gccsa_2026;
 
 ALTER TABLE admin_bdys.abs_2026_gccsa ADD CONSTRAINT abs_2026_gccsa_pk PRIMARY KEY (gid);
 CREATE INDEX abs_2026_gccsa_geom_idx ON admin_bdys.abs_2026_gccsa USING gist(geom);
