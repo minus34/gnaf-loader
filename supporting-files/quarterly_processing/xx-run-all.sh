417d7d9f552ff25d546c76d7682574c7ba611fae
Hugh Saalmans <hugh.saalmans@gmail.com>
2026-08-27 13:19:03 +1000

    reverted to run all processing

--- supporting-files/quarterly_processing/xx-run-all.sh
+++ supporting-files/quarterly_processing/xx-run-all.sh
@@ -3,8 +3,8 @@
 # get the directory this script is running from
 MAIN_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# cd "${MAIN_SCRIPT_DIR}" || exit
-# . 01_setup_conda_env.sh
+cd "${MAIN_SCRIPT_DIR}" || exit
+. 01_setup_conda_env.sh
 
 cd "${MAIN_SCRIPT_DIR}" || exit
 . 02-run-gnaf-loader-locality-clean-and-copy-to-aws-s3.sh
@@ -22,3 +22,6 @@ cd "${MAIN_SCRIPT_DIR}" || exit
 # URLs
 # https://github.com/minus34/gnaf-loader
 # https://hub.docker.com/r/minus34/gnafloader
+
+# Fix PROJ "transform: File not found or invalid" issue with GDA2020 (caused by incomplete PostGIS installation in Postgres.App)
+# cp -R "/opt/homebrew/Cellar/proj/9.8.1/share/proj/" "/Applications/Postgres.app/Contents/Versions/16/share/proj"
